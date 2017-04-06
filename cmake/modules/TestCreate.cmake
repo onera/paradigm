5b7b1058e2bacb20f225bb24b60919c37fc5db62
Eric Quemerais <eric.quemerais@onera.fr>
2017-04-06 14:51:09 +0200

    Switch to CMake - Ok

--- cmake/modules/TestCreate.cmake
+++ cmake/modules/TestCreate.cmake
@@ -27,22 +27,17 @@ endfunction()
 
 function(test_fortran_create name)
    add_executable(${name} "${name}.f90")
-   if ((NOT MPI_C_COMPILER) AND MPI_C_COMPILE_FLAGS)
-     set_target_properties(${name}
-                           PROPERTIES
-                           COMPILE_FLAGS ${MPI_C_COMPILE_FLAGS})
-   endif()
    if ((NOT MPI_Fortran_COMPILER) AND MPI_C_COMPILE_FLAGS)
-     set_target_properties(pdm_t_writer_ensight_3d
+     set_target_properties(${name}
                            PROPERTIES
                            COMPILE_FLAGS ${MPI_Fortran_COMPILE_FLAGS})
    endif()
-   set_target_properties(${name} PROPERTIES COMPILE_FLAGS ${MPI_C_COMPILE_FLAGS})
    target_include_directories(${name} PRIVATE ${CMAKE_SOURCE_DIR}
                                       PRIVATE ${CMAKE_BINARY_DIR}
                                       PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
    target_include_directories(${name} PRIVATE ${TEST_INC})
    target_link_libraries(${name} ${LINK_LIBRARIES})
+   set_target_properties(${name} PROPERTIES LINKER_LANGUAGE "Fortran")
    install(TARGETS ${name} RUNTIME DESTINATION bin)
    add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${MPIEXEC_MAX_NUMPROCS}
              ${MPIEXEC_PREFLAGS}
@@ -50,3 +45,11 @@ function(test_fortran_create name)
              ${MPIEXEC_POSTFLAGS})
 endfunction()
 
+function(test_python_create name)
+  file (COPY ${CMAKE_CURRENT_SOURCE_DIR}/${name}.py DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+  add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${MPIEXEC_MAX_NUMPROCS}
+            ${MPIEXEC_PREFLAGS}
+            python ${CMAKE_CURRENT_BINARY_DIR}/${name}.py
+            ${MPIEXEC_POSTFLAGS})
+
+endfunction()
--- cmake/modules/default_flags.cmake
+++ cmake/modules/default_flags.cmake
@@ -8,6 +8,8 @@
 # Fortran default flags
 #------------------------------------------------------------------------------
 
+message ("Fortran flags : ${CMAKE_Fortran_FLAGS}")
+
 if (NOT CMAKE_Fortran_FLAGS)
 
 if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
@@ -57,6 +59,7 @@ elseif (CMAKE_Fortran_COMPILER_ID MATCHES "XL")
   set (CMAKE_Fortran_FLAGS_MINSIZEREL      "-O3")
   
   set(FORTRAN_LIBRARIES xl xlf90_r xlsmp xlopt ${FORTRAN_LIBRARIES})
+  #cmake_host_system_information(RESULT HOSTNAME QUERY HOSTNAME)
   link_directories(/opt/ibmcmp/xlsmp/3.1/lib64 /opt/ibmcmp/vacpp/12.1/lib64 /opt/ibmcmp/xlf/14.1/lib64)
 
 elseif (CMAKE_Fortran_COMPILER_ID STREQUAL "PGI")
