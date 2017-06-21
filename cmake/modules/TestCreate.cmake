2db8e2aae3fa76c6a7021a1f75c12b59e53b5b85
Eric Quemerais <eric.quemerais@onera.fr>
2017-06-21 15:34:28 +0200

    Paradigm : optim test_c_create 2

--- cmake/modules/TestCreate.cmake
+++ cmake/modules/TestCreate.cmake
@@ -6,7 +6,7 @@
 #
 ################################################################################
 
-function(test_c_create name)
+function(test_c_create name n_proc)
    add_executable(${name} "${name}.c")
    if ((NOT MPI_C_COMPILER) AND MPI_C_COMPILE_FLAGS)
      set_target_properties(${name}
@@ -19,13 +19,13 @@ function(test_c_create name)
    target_include_directories(${name} PRIVATE ${TEST_INC})
    target_link_libraries(${name} ${LINK_LIBRARIES})
    install(TARGETS ${name} RUNTIME DESTINATION bin)
-   add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${MPIEXEC_MAX_NUMPROCS}
+   add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${n_proc}
              ${MPIEXEC_PREFLAGS}
              ${CMAKE_CURRENT_BINARY_DIR}/${name}
              ${MPIEXEC_POSTFLAGS})
 endfunction()
 
-function(test_fortran_create name)
+function(test_fortran_create name n_proc)
    add_executable(${name} "${name}.f90")
    if ((NOT MPI_Fortran_COMPILER) AND MPI_C_COMPILE_FLAGS)
      set_target_properties(${name}
@@ -39,15 +39,15 @@ function(test_fortran_create name)
    target_link_libraries(${name} ${LINK_LIBRARIES})
    set_target_properties(${name} PROPERTIES LINKER_LANGUAGE "Fortran")
    install(TARGETS ${name} RUNTIME DESTINATION bin)
-   add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${MPIEXEC_MAX_NUMPROCS}
+   add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${n_proc}
              ${MPIEXEC_PREFLAGS}
              ${CMAKE_CURRENT_BINARY_DIR}/${name}
              ${MPIEXEC_POSTFLAGS})
 endfunction()
 
-function(test_python_create name)
+function(test_python_create name n_proc)
   file (COPY ${CMAKE_CURRENT_SOURCE_DIR}/${name}.py DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-  add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${MPIEXEC_MAX_NUMPROCS}
+  add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${n_proc}
             ${MPIEXEC_PREFLAGS}
             python ${CMAKE_CURRENT_BINARY_DIR}/${name}.py
             ${MPIEXEC_POSTFLAGS})
