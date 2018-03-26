0fe03d477bfd7b4caefadf38940e25e4583601eb
Eric Quémerais <eric.quemerais@onera.fr>
2018-03-26 11:33:17 +0200

    Add distance interface

---
+++ src/mesh/pdm_mesh_dist.h
@@ -0,0 +1,130 @@
+#ifndef PDM_MESH_DIST_H
+#define PDM_MESH_DIST_H
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "pdm.h"
+#include "pdm_mpi.h"
+
+/*----------------------------------------------------------------------------*/
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*============================================================================
+ * Macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Type definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Public function definitions
+ *============================================================================*/
+
+
+/**
+ *
+ * \brief Create a structure to compute distance to a mesh nodal
+ *
+ * \param [in]   mesh_nodal_id  Mesh nodal identifier
+ * \param [in]   n_point_cloud  Number of point cloud
+ * \param [in]   comm           MPI communicator
+ *
+ * \return     Identifier
+ */
+
+int
+PDM_mesh_dist_create
+(
+ const int mesh_nodal_id,
+ const int n_point_cloud,
+ const PDM_MPI_Comm comm
+);
+
+
+/**
+ *
+ * \brief Set a point cloud
+ *
+ * \param [in]   id              Identifier
+ * \param [in]   i_point_cloud   Index of point cloud
+ * \param [in]   n_points        Number of points
+ * \param [in]   coords          Point coordinates
+ *
+ */
+
+void
+PDM_mesh_dist_cloud_set
+(
+ const int          id,
+ const int          i_point_cloud,
+ const int          n_points,
+ const double      *coords
+);
+
+
+/**
+ *
+ * \brief Process merge points
+ *
+ * \param [in]   id  Identifier
+ *
+ */
+
+void
+PDM_mesh_dist_process
+(
+ const int id
+);
+
+
+/**
+ *
+ * \brief Get mesh distance
+ *
+ * \param [in]   id              Identifier
+ * \param [in]   i_point_cloud   Current cloud
+ * \param [out]  dist            Distance
+ * \param [out]  proj            Projected point coordinates
+ * \param [out]  closest_part    Closest partition
+ * \param [out]  closest_elt     Closest element
+ *
+ */
+
+void
+PDM_mesh_dist_get
+(
+ const int       id,
+ const int       i_point_cloud,
+       double  **dist,
+       double  **proj,
+       int     **closest_part,
+       int     **closest_elt,
+);
+
+
+/**
+ *
+ * \brief Free a distance mesh structure
+ *
+ * \param [in]  id  Identifier
+ *
+ * \return     Identifier
+ */
+
+int
+PDM_mesh_dist_free
+(
+ const int id
+);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif // PDM_MESH_DIST_H
