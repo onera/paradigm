0f1f81964895dd29d8a8ef3a5a01145473a93387
Eric Quémerais <eric.quemerais@onera.fr>
2019-04-07 21:07:57 +0200

    mesh_location : API location dbbtree

--- src/struct/pdm_dbbtree.h
+++ src/struct/pdm_dbbtree.h
@@ -176,6 +176,31 @@ PDM_dbbtree_closest_upper_bound_dist_boxes_get
  );
 
 
+/**
+ *
+ * Get the location of a point cloud
+ *
+ *   \param [in] bt               Pointer to box tree structure
+ *   \param [in] n_pts            Number of points
+ *   \param [in] pts              Point coordinates (size = 3 * n_pts)
+ *   \param [in] pts_g_num        Point global numbers
+ *   \param [out] box_index       Index of boxes (size = n_pts + 1)
+ *   \param [out] box_g_num       Global num of boxes (size = i_boxes[n_pts])
+ *
+ */
+
+void
+PDM_dbbtree_location_boxes_get
+(
+PDM_dbbtree_t    *dbbt,
+const int        n_pts,        
+double           pts[],
+PDM_g_num_t      pts_g_num[],
+int             *box_index[],  
+PDM_g_num_t     *box_g_num[]
+);
+
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
