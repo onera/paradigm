1390ac9612cfd1680083851c12f6c8ad17881129
Eric Quemerais <eric.quemerais@onera.fr>
2019-04-09 16:32:06 +0200

    pdm_para_octree  : API + types

--- src/struct/pdm_octree.c
+++ src/struct/pdm_octree.c
@@ -85,26 +85,6 @@ typedef struct {
 } _box_tree_stats_t;
 
 
-//
-///**
-// * \struct _octant_t
-// * \brief  Define an octant
-// *
-// */
-//
-//typedef struct  {
-//
-//  int  ancestor_id; /*!< Ids of ancestor in octree array */
-//  PDM_octree_child_t  location_in_ancestor; /*!< Location in ancestor */
-//  int  depth;       /*!< Depth in the tree */
-//  int  children_id[8]; /*!< Ids of children in octree array */
-//  int  idx[9];         /*!< Start index of point list for each octant */
-//  int  n_points;       /*!< Number of points in octant*/
-//  double extents[6];   /*!< Extents of the node */
-//
-//} _octant_t;
-
-
 /**
  * \struct _octree_t
  * \brief  Define an octree
@@ -113,9 +93,7 @@ typedef struct {
 
 typedef struct  {
   int    octree_seq_id;             /*!< Identifier of the associated octree seq */
-//  double  extents[6];            /*!< Extents of current process */
-//  double *extents_proc;          /*!< Extents of processes */
-//  int    depth_max;              /*!< Maximum depth of the three */
+
   PDM_MPI_Comm comm;             /*!< MPI communicator */
   PDM_MPI_Comm rankComm;             /*!< MPI communicator */
 
@@ -138,20 +116,6 @@ typedef struct  {
   int            *n_points;   /*!< Number of points */
   PDM_g_num_t   **g_num;      /*!< Point global number */
 
-//  int points_in_leaf_max;        /*!< Maximum number of points in a leaf */
-//  double tolerance;              /*!< Relative geometric tolerance */
-//  int   n_nodes;                 /*!< Current number of nodes in octree */
-//  int   n_nodes_max;             /*!< Maximum number of nodes in octree */
-//  int   *n_points;               /*!< Number of points in each cloud */
-//  int   t_n_points;              /*!< total number of points */
-//  int   n_point_clouds;          /*!< Number of point cloud */
-//  const double **point_clouds;         /*!< points cloud */
-//  int *point_ids;                /*!< Id's of points in it cloud sorted by octree
-//                                      (size: n_points + 1) */
-//  int *point_icloud;             /*!< Cloud's of points sorted by octree
-//                                      (size: n_points + 1) */
-//  _octant_t   *nodes;            /*!< Array of octree nodes
-//                                      (size: n_nodes_max) */
 } _octree_t;
 
 /*============================================================================
