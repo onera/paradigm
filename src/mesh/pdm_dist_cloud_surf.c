d941e352b0b24c854c3d6661dac4d28f5d033c33
Eric Quemerais <eric.quemerais@onera.fr>
2019-06-25 12:05:12 +0200

    BUGFIX: pdm_dist_cloud_surf (depth_max too large)

--- src/mesh/pdm_dist_cloud_surf.c
+++ src/mesh/pdm_dist_cloud_surf.c
@@ -502,7 +502,7 @@ PDM_dist_cloud_surf_compute
     PDM_timer_resume(dist->timer);
 
     const double tolerance = 1e-4;
-    const int depth_max = 1000;
+    const int depth_max = 35;
     const int points_in_leaf_max = 4;
 
     int n_part_mesh = 0;
