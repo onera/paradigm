4147b68cbe424952f56ca5437bf9eee4d01c2193
Eric Quemerais <eric.quemerais@onera.fr>
2019-07-24 16:25:11 +0200

    PDM_block_to_part : split sendbuffer if too big

--- src/struct/pdm_block_to_part_priv.h
+++ src/struct/pdm_block_to_part_priv.h
@@ -60,6 +60,7 @@ typedef struct {
   int           *distributed_data;    /*!< Distributed data for each process
                                     * (size : requestd_data_idx[s_comm - 1]
                                              +requestd_data_n[s_comm - 1] ) */
+  int           pttopt_comm;         /*!< Use point to point communication if pttopt_comm == 1 */
 
 } _pdm_block_to_part_t;
 
@@ -75,4 +76,3 @@ typedef struct {
 #endif
 
 #endif	/* PDM_BLOCK_TO_PART_PRIV_H */
-
