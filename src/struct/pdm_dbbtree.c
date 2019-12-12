11b63a2600e8e8052098de23927ce23d62d0b236
Bastien Andrieu <bastien.andrieu@yahoo.fr>
2019-12-12 09:41:50 +0100

    seuil 1.2, max copies 15%

--- src/struct/pdm_dbbtree.c
+++ src/struct/pdm_dbbtree.c
@@ -1200,8 +1200,8 @@ PDM_dbbtree_closest_upper_bound_dist_boxes_get
   /*
    * PARAMETERS
    */
-  double RANK_COPY_threshold  = 1.5;
-  double RANK_COPY_max_copies = 0.1;
+  double RANK_COPY_threshold  = 1.2;
+  double RANK_COPY_max_copies = 0.15;
 
 
 
