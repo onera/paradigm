1c29be4f3454271f0c71db1a9c94be0b9b05a6ac
Bruno Maugars <bruno.maugars@onera.fr>
2020-02-27 09:26:09 +0100

    [PDM_distant_neighbor] - Add test - Step 2

--- test/pdm_t_distant_neighbor.c
+++ test/pdm_t_distant_neighbor.c
@@ -43,6 +43,8 @@ char *argv[]
    *    I/  Localisation in the frame of the current/opposite boundary condition
    *    II/ Use distant_neighbor to find out the face number in the partition
    */
+  int point_list_j1[15] = {145, 153, 155, 163, 172, 180, 201, 206, 207, 211, 212, 215, 217, 222, 227};
+
   double xyz_j1[45] = {0.5,  2.5,  4. ,  0.5,  1.5,  4. ,  1.5,  2.5,  4. ,  1.5,  1.5,
                        4. ,  2.5,  1.5,  4. ,  3.5,  1.5,  4. ,  1.5,  0.5,  4. ,  2.5,
                        0.5,  4. ,  2.5,  2.5,  4. ,  3.5,  0.5,  4. ,  3.5,  2.5,  4. ,
@@ -50,6 +52,8 @@ char *argv[]
                        4. };
 
   double cln_j1[15] = {1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 1., 1.};
+
+  int point_list_j2[15] = {136, 142, 150, 151, 160, 161, 170, 171, 188, 191, 194, 199, 205, 214, 221};
 
   double xyz_j2[45] = {0.5,  1.5,  4. ,  1.5,  1.5,  4. ,  1.5,  2.5,  4. ,  2.5,  1.5,
                        4. ,  2.5,  2.5,  4. ,  3.5,  1.5,  4. ,  3.5,  2.5,  4. ,  4.5,
@@ -115,6 +119,7 @@ char *argv[]
   /*
    * Get resulting points_merge
    */
+  int  *n_entity        = (int * ) malloc( n_cloud * sizeof(int* ));
   int **candidates_idx  = (int **) malloc( n_cloud * sizeof(int**));
   int **candidates_desc = (int **) malloc( n_cloud * sizeof(int**));
   for(int i_cloud = 0; i_cloud < n_cloud; i_cloud++){
@@ -122,6 +127,8 @@ char *argv[]
     int n_desc   = -1;
     PDM_points_merge_candidates_get(pm_id, i_cloud, &candidates_idx[i_cloud], &candidates_desc[i_cloud]);
     PDM_points_merge_candidates_size_get(pm_id, i_cloud, &n_cloud_points, &n_desc);
+
+    n_entity[i_cloud] = n_cloud_points;
 
     assert(n_desc == candidates_idx[i_cloud][n_cloud_points]);
     if(1 == 1){
@@ -141,14 +148,56 @@ char *argv[]
   }
 
   /*
+   *  Now we have the connection between the two cloud (in this case it's a perfect match )
+   *  We need to find out the connection in the partition (not in the cloud )
+   *  To do that we setup an exchange with distant neighbor with the local face of each partition
+   */
+  int pdn_id = PDM_distant_neighbor_create(PDM_MPI_COMM_WORLD,
+                                           n_cloud,
+                                           n_entity,
+                                           candidates_idx,
+                                           candidates_desc);
+
+  /*
+   *  SetUp exchange
+   */
+  int** send_entity_data = (int **) malloc( n_cloud * sizeof(int**));
+  if(nRank == 1){
+    send_entity_data[0] = point_list_j1;
+    send_entity_data[1] = point_list_j2;
+  } else if(nRank == 1){
+    if(iRank == 0){
+      send_entity_data[0] = point_list_j1;
+    } else if(iRank == 1){
+      send_entity_data[0] = point_list_j2;
+    }
+  }
+
+  int stride = 1;
+  int** recv_entity_data = NULL;
+  PDM_distant_neighbor_exch(pdn_id,
+                            sizeof(int),
+                            PDM_STRIDE_CST,
+                            stride,
+                            NULL,
+                            send_entity_data,
+                            NULL,
+                   (int**) &recv_entity_data);
+
+
+
+  /*
    * Free
    */
+  PDM_distant_neighbor_free(pdn_id);
   PDM_points_merge_free(pm_id);
   free(coords);
   free(char_lenght);
   free(candidates_idx);
   free(candidates_desc);
-  PDM_MPI_Finalize ();
+  free(n_entity);
+  free(send_entity_data);
+  PDM_MPI_Finalize();
 
   PDM_printf ("\nfin Test\n");
 
