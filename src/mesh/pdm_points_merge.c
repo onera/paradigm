ef4f48190c4ec9cd66e4051dbcbe6dde8340791b
Bruno Maugars <bruno.maugars@onera.fr>
2020-02-27 09:03:14 +0100

    [PDM_distant_neighbor] - Add test - Step 1

--- src/mesh/pdm_points_merge.c
+++ src/mesh/pdm_points_merge.c
@@ -1057,7 +1057,7 @@ PDM_points_merge_candidates_get
   *candidates_idx  = ppm->candidates_idx[i_point_cloud];
   *candidates_desc = ppm->candidates_desc[i_point_cloud];
 
-  if (0 == 1) {
+  if (1 == 1) {
     printf("candidates : \n");
     for (int i = 0; i < ppm->n_points[i_point_cloud]; i++) {
       if (ppm->candidates_idx[i_point_cloud][i+1] > ppm->candidates_idx[i_point_cloud][i]) {
