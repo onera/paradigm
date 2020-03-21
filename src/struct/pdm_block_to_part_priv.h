60344f448190b30180be62e30c4d23bcda9a3607
Bruno Maugars <bruno.maugars@onera.fr>
2020-03-21 18:50:56 +0100

    [PDM_block_to_part] Cosmetics camelCase --> snake_case - Clean

--- src/struct/pdm_block_to_part_priv.h
+++ src/struct/pdm_block_to_part_priv.h
@@ -41,35 +41,32 @@ extern "C" {
  */
 
 typedef struct {
-  PDM_g_num_t    *blockDistribIdx;  /*!< Block distribution
-                                    * (size : \ref size of \ref comm + 1) */
-  PDM_MPI_Comm       comm;             /*!< MSG communicator */
-  int            s_comm;           /*!< Communicator size */
-  int            myRank;           /*!< Current rank in comm */
-  int            n_part;           /*!< Number of partitions */
-  int           *n_elt;            /*!< Number of elements for each partition */
-  int          **ind;              /*!< Ind for each element partition in distributed_data */
-  int           *requested_data_n; /*!< Numer of requested data for each process index
-                                    * (size : s_comm) */
-  int           *requested_data_idx;/*!< Requested data for each process index
-                                    * (size : s_comm) */
-  int           *distributed_data_n;/*!< Numer of distributed data for each process index
-                                    * (size : s_comm) */
+  PDM_g_num_t   *block_distrib_idx;   /*!< Block distribution
+                                       * (size : \ref size of \ref comm + 1) */
+  PDM_MPI_Comm   comm;                /*!< MSG communicator */
+  int            n_rank;              /*!< Communicator size */
+  int            i_rank;              /*!< Current rank in comm */
+  int            n_part;              /*!< Number of partitions */
+  int           *n_elt;               /*!< Number of elements for each partition */
+  int          **ind;                 /*!< Ind for each element partition in distributed_data */
+  int           *requested_data_n;    /*!< Numer of requested data for each process index
+                                       * (size : n_rank) */
+  int           *requested_data_idx;  /*!< Requested data for each process index
+                                       * (size : n_rank) */
+  int           *distributed_data_n;  /*!< Numer of distributed data for each process index
+                                       * (size : n_rank) */
   int           *distributed_data_idx;/*!< Distributed data for each process index
-                                    * (size : s_comm) */
+                                       * (size : n_rank) */
   int           *distributed_data;    /*!< Distributed data for each process
-                                    * (size : requestd_data_idx[s_comm - 1]
-                                             +requestd_data_n[s_comm - 1] ) */
-  int           pttopt_comm;         /*!< Use point to point communication if pttopt_comm == 1 */
+                                       * (size : requestd_data_idx[n_rank - 1]
+                                             +requestd_data_n[n_rank - 1] ) */
+  int           pttopt_comm;          /*!< Use point to point communication if pttopt_comm == 1 */
 
 } _pdm_block_to_part_t;
 
 /*=============================================================================
  * Static global variables
  *============================================================================*/
-
-
-
 
 #ifdef	__cplusplus
 }
