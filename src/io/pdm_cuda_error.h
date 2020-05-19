40bac031f8b7e6b1e12d56ced31fdf5368a295b0
Adrien Lafontan <adrien.lafontan@icloud.com>
2020-05-19 17:35:50 +0200

    Add error handling header for cuda calls

---
+++ src/io/pdm_cuda_error.h
@@ -0,0 +1,40 @@
+#ifndef __PDM_CUDA_ERROR_H__
+#define __PDM_CUDA_ERROR_H__
+
+/*============================================================================
+ * CUDA error handling
+ *============================================================================*/
+
+/*
+  This file is part of the CWIPI library.
+
+  Copyright (C) 2017 ONERA
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*-----------------------------------------------------------------------------*/
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+#endif /* __PDM_CUDA_ERROR_H__ */
