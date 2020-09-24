f819e2647460f58a589ad8ad79051ef898798f7a
Eric Quemerais <eric.quemerais@onera.fr>
2020-09-24 09:07:09 +0200

    [iso_c_binding] add closest_points - 1

--- src/mesh/pdm_mesh_location.F90
+++ src/mesh/pdm_mesh_location.F90
@@ -1,7 +1,7 @@
 !-----------------------------------------------------------------------------
 ! This file is part of the ParaDiGM library.
 !
-! Copyright (C) 2019  ONERA
+! Copyright (C) 2020  ONERA
 !
 ! This library is free software; you can redistribute it and/or
 ! modify it under the terms of the GNU Lesser General Public
