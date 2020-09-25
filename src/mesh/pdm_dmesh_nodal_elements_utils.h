1b4eabddc3aecb9b5b053be0133a2851251d93e9
Bruno Maugars <bruno.maugars@onera.fr>
2020-09-25 12:48:30 +0200

    [pdm_dmesh_nodal] Mv methods

--- src/mesh/pdm_dmesh_nodal_elements_utils.h
+++ src/mesh/pdm_dmesh_nodal_elements_utils.h
@@ -18,12 +18,15 @@ extern "C" {
 
 
 void
-PDM_decompose
+PDM_hexa_section_decompose_elemt_to_face
 (
- PDM_g_num_t  n_vtx,
- PDM_g_num_t  n_cell
+      PDM_g_num_t  n_elmt,
+const PDM_g_num_t *elmt_vtx,
+      int         *elmt_face_vtx_idx,
+      PDM_g_num_t *elmt_face_vtx,
+      PDM_g_num_t *elmt_face_cell,
+      PDM_g_num_t *elmt_cell_face
 );
-
 
 
 #ifdef __cplusplus
