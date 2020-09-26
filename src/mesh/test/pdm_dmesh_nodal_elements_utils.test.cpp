e6183d6702937f0bce0200f832c9c0619c9bb205
Bruno Maugars <bruno.maugars@onera.fr>
2020-09-26 15:11:27 +0200

    [pdm_dmesh_nodal] Prepare test - Correction vtx

--- src/mesh/test/pdm_dmesh_nodal_elements_utils.test.cpp
+++ src/mesh/test/pdm_dmesh_nodal_elements_utils.test.cpp
@@ -11,14 +11,15 @@ MPI_TEST_CASE("decomposes hexa ",1) {
   const int         n_hexa_section_1 = 10;
   PDM_g_num_t connec_hexa_1[n_hexa_section_1] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-  int mesh_id = PDM_DMesh_nodal_create(test_comm, n_vtx, n_cell);
+  PDM_MPI_Comm pdm_comm = PDM_MPI_mpi_2_pdm_mpi_comm(&test_comm);
+  int mesh_id = PDM_DMesh_nodal_create(pdm_comm, n_vtx, n_cell);
 
   int hexa_section_1 = PDM_DMesh_nodal_section_add(mesh_id, PDM_MESH_NODAL_HEXA8);
 
-  // PDM_DMesh_nodal_section_std_set(mesh_id,
-  //                                 hexa_section_1,
-  //                                 n_hexa_section_1,
-  //                                 connec_hexa_1);
+  PDM_DMesh_nodal_section_std_set(mesh_id,
+                                  hexa_section_1,
+                                  n_hexa_section_1,
+                                  connec_hexa_1);
 
   PDM_DMesh_nodal_free(mesh_id);
 }
