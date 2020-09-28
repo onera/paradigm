f81c2e917b512f6ec2c18de924aebbad4019e12a
Bruno Maugars <bruno.maugars@onera.fr>
2020-09-28 18:25:54 +0200

    [pdm_dmesh_nodal] Add pyramid test

--- src/mesh/test/pdm_dmesh_nodal_elements_utils.test.cpp
+++ src/mesh/test/pdm_dmesh_nodal_elements_utils.test.cpp
@@ -203,3 +203,100 @@ MPI_TEST_CASE("decomposes tetra ",1) {
 
   PDM_DMesh_nodal_free(dmesh_nodal_id);
 }
+
+
+// double coord_pyra_x[9] = {0., 1., 0., 1., 0., 1., 0., 1., 0.5};
+// double coord_pyra_y[9] = {0., 0., 1., 1., 0., 0., 1., 1., 0.5};
+// double coord_pyra_z[9] = {0., 0., 0., 0., 1., 1., 1., 1., 0.5};
+
+MPI_TEST_CASE("decomposes pyra ",1) {
+  const PDM_g_num_t n_vtx             = 9;
+  const PDM_g_num_t n_cell            = 6;
+  const int         n_pyra_section_1 = 6;
+  PDM_g_num_t connec_pyra_1[30] = {1, 2, 4, 3, 9,
+                                   5, 7, 8, 6, 9,
+                                   1, 3, 7, 5, 9,
+                                   2, 6, 8, 4, 9,
+                                   1, 5, 6, 2, 9,
+                                   3, 4, 8, 7, 9};
+
+  PDM_MPI_Comm pdm_comm = PDM_MPI_mpi_2_pdm_mpi_comm(&test_comm);
+  int dmesh_nodal_id = PDM_DMesh_nodal_create(pdm_comm, n_vtx, n_cell);
+
+  int pyra_section_1 = PDM_DMesh_nodal_section_add(dmesh_nodal_id, PDM_MESH_NODAL_PYRAMID5);
+
+  PDM_DMesh_nodal_section_std_set(dmesh_nodal_id,
+                                  pyra_section_1,
+                                  n_pyra_section_1,
+                                  connec_pyra_1);
+  int n_face_elt_tot     = -1;
+  int n_sum_vtx_face_tot = -1;
+  PDM_dmesh_nodal_decompose_faces_get_size(dmesh_nodal_id, &n_face_elt_tot, &n_sum_vtx_face_tot);
+
+  printf("n_face_elt_tot     = %i\n", n_face_elt_tot);
+  printf("n_sum_vtx_face_tot = %i\n", n_sum_vtx_face_tot);
+
+  CHECK( n_face_elt_tot     == 30 );
+  CHECK( n_sum_vtx_face_tot == 96 );
+
+  // PDM_g_num_t* delmt_face_cell    = (PDM_g_num_t*) malloc(  n_face_elt_tot     * sizeof(PDM_g_num_t));
+  // int*         dcell_face_vtx_idx = (int        *) malloc( (n_face_elt_tot +1) * sizeof(int        ));
+  // PDM_g_num_t* dcell_face_vtx     = (PDM_g_num_t*) malloc(  n_sum_vtx_face_tot * sizeof(PDM_g_num_t));
+
+  std::vector<int>         dcell_face_vtx_idx(n_face_elt_tot +1);
+  std::vector<PDM_g_num_t> dcell_face_vtx(n_sum_vtx_face_tot);
+  std::vector<PDM_g_num_t> delmt_face_cell(n_face_elt_tot);
+
+  dcell_face_vtx_idx[0] = 0;
+  PDM_dmesh_nodal_decompose_faces(dmesh_nodal_id,
+                                  dcell_face_vtx_idx.data(),
+                                  dcell_face_vtx.data(),
+                                  delmt_face_cell.data(),
+                                  NULL);
+  // dcell_face_vtx_idx[0] = 0;
+  // PDM_dmesh_nodal_decompose_faces(dmesh_nodal_id,
+  //                                 dcell_face_vtx_idx,
+  //                                 dcell_face_vtx,
+  //                                 delmt_face_cell,
+  //                                 NULL);
+
+  PDM_log_trace_array_long(delmt_face_cell.data()  , n_face_elt_tot    , "delmt_face_cell:: ");
+  PDM_log_trace_array_int(dcell_face_vtx_idx.data(), n_face_elt_tot+1  , "dcell_face_vtx_idx:: ");
+  PDM_log_trace_array_long(dcell_face_vtx.data()   , n_sum_vtx_face_tot, "dcell_face_vtx:: ");
+
+  std::vector<PDM_g_num_t> delmt_face_cell_expected    = {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6};
+  std::vector<int>         dcell_face_vtx_idx_expected = {0, 4, 7, 10, 13, 16, 20, 23, 26, 29, 32, 36, 39, 42, 45, 48, 52, 55, 58, 61, 64, 68, 71, 74, 77, 80, 84, 87, 90, 93, 96};
+  std::vector<PDM_g_num_t> dcell_face_vtx_expected     = {3 ,4, 2, 1, 9, 1, 2, 9, 2, 4, 9, 4, 3, 9, 3, 1, 6, 8, 7, 5, 9, 5, 7, 9, 7, 8, 9, 8, 6, 9, 6, 5, 5, 7, 3, 1, 9, 1, 3, 9, 3, 7, 9, 7, 5, 9, 5, 1, 4, 8, 6, 2, 9, 2, 6, 9, 6, 8, 9, 8, 4, 9, 4, 2, 2, 6, 5, 1, 9, 1, 5, 9, 5, 6, 9, 6, 2, 9, 2, 1, 7, 8, 4, 3, 9, 3, 4, 9, 4, 8, 9, 8, 7, 9, 7, 3};
+
+  CHECK( delmt_face_cell    == delmt_face_cell_expected);
+  CHECK( dcell_face_vtx_idx == dcell_face_vtx_idx_expected);
+  CHECK( dcell_face_vtx     == dcell_face_vtx_expected);
+
+  PDM_DMesh_nodal_cell_face_compute(dmesh_nodal_id);
+
+  // free(delmt_face_cell);
+  // free(dcell_face_vtx_idx);
+  // free(dcell_face_vtx);
+  PDM_g_num_t* dface_cell;
+  PDM_g_num_t* dface_vtx;
+  int*         dface_vtx_idx;
+  int dn_face = PDM_DMesh_nodal_face_cell_get(dmesh_nodal_id, &dface_cell);
+  PDM_DMesh_nodal_face_vtx_get(dmesh_nodal_id, &dface_vtx_idx, &dface_vtx);
+
+  CHECK( dn_face == 18);
+
+  PDM_g_num_t dface_cell_expected[38]    = {1, 0, 1, 5, 3, 1, 5, 0, 4, 1, 5, 3, 3, 0, 1, 6, 5, 4, 3, 6, 5, 2, 4, 0, 4, 6, 3, 2, 6, 0, 2, 4, 2, 6, 2, 0};
+  int         dface_vtx_idx_expected[19] = {0, 4, 7, 10, 14, 17, 20, 24, 27, 30, 33, 36, 40, 43, 46, 50, 53, 56, 60 };
+  PDM_g_num_t dface_vtx_expected[60]     = {3, 4, 2, 1, 9, 1, 2, 9, 1, 3, 2, 6, 5, 1, 9, 4, 2, 9, 1, 5, 5, 7, 3, 1, 9, 4, 3, 9, 6, 2, 9, 3, 7, 9, 5, 6, 4, 8, 6, 2, 9, 8, 4, 9, 7, 5, 7, 8, 4, 3, 9, 8, 6, 9, 7, 8, 6, 8, 7, 5};
+
+  MPI_CHECK_EQ_C_ARRAY(0, dface_cell   , dface_cell_expected   , 2*dn_face             );
+  MPI_CHECK_EQ_C_ARRAY(0, dface_vtx_idx, dface_vtx_idx_expected, dn_face+1             );
+  MPI_CHECK_EQ_C_ARRAY(0, dface_vtx    , dface_vtx_expected    , dface_vtx_idx[dn_face]);
+
+  // printf(" dface_vtx_idx[dn_face]::%i\n",  dface_vtx_idx[dn_face]);
+  // PDM_log_trace_array_long(dface_cell, 2*dn_face, "dface_cell:: ");
+  // PDM_log_trace_array_int(dface_vtx_idx, dn_face+1, "dface_vtx_idx:: ");
+  // PDM_log_trace_array_long(dface_vtx, dface_vtx_idx[dn_face], "dface_vtx:: ");
+
+  PDM_DMesh_nodal_free(dmesh_nodal_id);
+}
