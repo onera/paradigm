23f09e13baa567637cd99c6bf8f55ff85ca76c7e
Eric Quemerais <eric.quemerais@onera.fr>
2020-09-30 16:41:27 +0200

    add pdm_t_plane_intersect_unit_f + BUFIX into pdm_gnum (Case 0 point)*

--- test/pdm_t_mesh_location_f.F90
+++ test/pdm_t_mesh_location_f.F90
@@ -1,7 +1,7 @@
 !-----------------------------------------------------------------------------
-! This file is part of the CWIPI library.
+! This file is part of the ParaDiGM library.
 !
-! Copyright (C) 2011  ONERA
+! Copyright (C) 2020  ONERA
 !
 ! This library is free software; you can redistribute it and/or
 ! modify it under the terms of the GNU Lesser General Public
