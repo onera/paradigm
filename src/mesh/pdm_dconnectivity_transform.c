00304c34d1ce454e0c092cbe69b559db06a9e485
Bruno Maugars <bruno.maugars@onera.fr>
2020-09-30 10:04:44 +0200

    [pdm_ppart/pdm_dconnectivity] Add // test

--- src/mesh/pdm_dconnectivity_transform.c
+++ src/mesh/pdm_dconnectivity_transform.c
@@ -17,6 +17,7 @@
 #include "pdm_mpi.h"
 #include "pdm_block_to_part.h"
 #include "pdm_dconnectivity_transform.h"
+#include "pdm_para_graph_dual.h"
 #include "pdm_error.h"
 #include "pdm_timer.h"
 #include "pdm_logging.h"
@@ -68,7 +69,6 @@ PDM_deduce_combine_connectivity
        PDM_g_num_t    **dcell_vtx
 )
 {
-
   int i_rank;
   int n_rank;
 
@@ -77,6 +77,16 @@ PDM_deduce_combine_connectivity
 
   int dn_cell = cell_distrib[i_rank+1] - cell_distrib[i_rank];
   int dn_face = face_distrib[i_rank+1] - face_distrib[i_rank];
+
+  /*
+   * Some print to create unit_test
+   */
+  PDM_log_trace_array_long(cell_distrib  , n_rank+1               , "cell_distrib::");
+  PDM_log_trace_array_long(face_distrib  , n_rank+1               , "face_distrib::");
+  PDM_log_trace_array_int (dcell_face_idx, dn_cell+1              , "dcell_face_idx::");
+  PDM_log_trace_array_long(dcell_face    , dcell_face_idx[dn_cell], "dcell_face::");
+  PDM_log_trace_array_int (dface_vtx_idx , dn_face+1              , "dface_vtx_idx::");
+  PDM_log_trace_array_long(dface_vtx     , dface_vtx_idx[dn_face] , "dface_vtx::");
 
   int* dface_vtx_n = (int * ) malloc( dn_face * sizeof(int));
   for(int i = 0; i < dn_face; ++i) {
@@ -159,7 +169,7 @@ PDM_deduce_combine_connectivity
 
     for(int i_face = 0; i_face < n_face_per_cell; ++i_face) {
       _dcell_vtx_idx[i_cell+1] += pface_vtx_n[0][idx];
-      dcell_vtx_n[i_cell]     += pface_vtx_n[0][idx++];
+      dcell_vtx_n[i_cell]      += pface_vtx_n[0][idx++];
     }
   }
   printf("idx::%i\n", idx);
@@ -167,18 +177,17 @@ PDM_deduce_combine_connectivity
 
   assert(idx == dcell_face_idx[dn_cell]);
 
-  *dcell_vtx = pface_vtx[0]; // Car 1 seule partition
+  PDM_g_num_t* _dcell_vtx = pface_vtx[0]; // Car 1 seule partition
 
   PDM_log_trace_array_int(_dcell_vtx_idx, dn_cell+1, "_dcell_vtx_idx::");
   PDM_log_trace_array_int(dcell_vtx_n  , dn_cell  , "dcell_vtx_n::");
 
-  PDM_para_graph_compress_connectivity2(dn_cell, dcell_vtx_idx, dcell_vtx_n, dcell_vtx);
+  PDM_para_graph_compress_connectivity2(dn_cell, _dcell_vtx_idx, dcell_vtx_n, _dcell_vtx);
 
   /*
    * Realloc
    */
-  *dcell_vtx = (PDM_g_num_t *) realloc(dcell_vtx, sizeof(PDM_g_num_t) * _dcell_vtx_idx[dn_cell] );
-  PDM_g_num_t* _dcell_vtx = *dcell_vtx;
+  _dcell_vtx = (PDM_g_num_t *) realloc(_dcell_vtx, sizeof(PDM_g_num_t) * _dcell_vtx_idx[dn_cell] );
 
   PDM_log_trace_array_int (_dcell_vtx_idx, dn_cell+1              , "after -> _dcell_vtx_idx::");
   PDM_log_trace_array_int (dcell_vtx_n   , dn_cell                , "after -> dcell_vtx_n::");
@@ -187,9 +196,13 @@ PDM_deduce_combine_connectivity
   /*
    * Free
    */
+  free(pface_vtx_n[0]);
+  free(pface_vtx_n);
   free(dcell_vtx_n);
   free(pface_vtx_idx);
   free(pface_vtx);
+
+  *dcell_vtx = _dcell_vtx;
 
 }
 
