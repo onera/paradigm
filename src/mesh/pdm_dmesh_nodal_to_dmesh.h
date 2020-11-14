cdd04551271660fdc300deda9025bcd63bd37903
Bruno Maugars <bruno.maugars@onera.fr>
2020-11-14 11:52:16 +0100

    [pdm_dmesh_nodal_to_dmesh] Prepare files

---
+++ src/mesh/pdm_dmesh_nodal_to_dmesh.h
@@ -0,0 +1,25 @@
+#ifndef __PDM_DMESH_NODAL_TO_DMESH_H__
+#define __PDM_DMESH_NODAL_TO_DMESH_H__
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ *  Header for the current file
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+extern "C" {
+#if 0
+} /* Fake brace to force back Emacs auto-indentation back to column 0 */
+#endif
+#endif /* __cplusplus */
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __PDM_MESH_NODAL_H__ */
