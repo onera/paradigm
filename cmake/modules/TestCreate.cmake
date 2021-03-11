ceaef37e7bdedf01b1e478059d9d0434274cbe61
Eric Quemerais <eric.quemerais@onera.fr>
2021-03-11 18:17:52 +0100

    Nuga : first test - 3

--- cmake/modules/TestCreate.cmake
+++ cmake/modules/TestCreate.cmake
@@ -87,3 +87,25 @@ function(test_cpp_unit_create name n_proc)
              ${CMAKE_CURRENT_BINARY_DIR}/${name}
              ${MPIEXEC_POSTFLAGS})
 endfunction()
+
+function(test_cpp_create name n_proc)
+   add_executable(${name} "${name}.cpp")
+   if ((NOT MPI_CXX_COMPILER) AND MPI_CXX_COMPILE_FLAGS)
+     set_target_properties(${name}
+                           PROPERTIES
+                           COMPILE_FLAGS ${MPI_C_COMPILE_FLAGS})
+   endif()
+   target_include_directories(${name} PRIVATE ${CMAKE_SOURCE_DIR}
+                                      PRIVATE ${CMAKE_BINARY_DIR}
+                                      PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
+   target_include_directories(${name} PRIVATE ${TEST_INC})
+   target_link_libraries(${name} ${LINK_LIBRARIES})
+   install(TARGETS ${name} RUNTIME DESTINATION bin)
+   add_test (${name} ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${n_proc}
+             ${MPIEXEC_PREFLAGS}
+             ${CMAKE_CURRENT_BINARY_DIR}/${name}
+             ${MPIEXEC_POSTFLAGS})
+endfunction()
+
+
+
