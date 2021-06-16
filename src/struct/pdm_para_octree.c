5e63d75ed92e914a1d676f7d3e8a4a0976c30d4c
Bastien Andrieu <bastien.andrieu@yahoo.fr>
2021-06-16 16:11:27 +0200

    avoid inspecting empty nodes - 2

--- src/struct/pdm_para_octree.c
+++ src/struct/pdm_para_octree.c
@@ -4284,7 +4284,8 @@ _single_closest_point
             prev_end = new_end;
             child_end[i] = new_end;
 
-            if (child_end[i] > child_start[i]) {
+            if (child_end[i] > child_start[i] &&
+                octants->range[child_start[i]] < octants->range[child_end[i]-1]) {
               child_dist[i] = _octant_min_dist2 (dim,
                                                  child_code[i],
                                                  d,
@@ -7260,16 +7261,16 @@ PDM_para_octree_single_closest_point
 
     if (USE_SHARED_OCTREE) {
       double *node_min_dist = (double *) malloc (sizeof(double) * n_pts);
-      /*PDM_box_tree_min_dist_max_box (bt_shared,
+      PDM_box_tree_min_dist_max_box (bt_shared,
                                      n_pts,
                                      pts_coord,
                                      rank_pt,
-                                     node_min_dist);*/
-      PDM_box_tree_min_dist_max_box_disjoint (bt_shared,
+                                     node_min_dist);
+      /*PDM_box_tree_min_dist_max_box_disjoint (bt_shared,
                                               n_pts,
                                               pts_coord,
                                               rank_pt,
-                                              node_min_dist);
+                                              node_min_dist);*/
       free (node_min_dist);
 
       for (int i = 0; i < n_pts; i++) {
