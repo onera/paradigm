ea71bc144228c5afabc14ca3379bc1a79abee049
Bastien Andrieu <bastien.andrieu@onera.fr>
2021-09-22 10:18:18 +0200

    add real field to PDM_vtk_write_std_elements

--- src/io/pdm_vtk.h
+++ src/io/pdm_vtk.h
@@ -119,6 +119,22 @@ PDM_vtk_write_std_elements
  );
 
 
+void
+PDM_vtk_write_std_elements_double
+(
+ const char                 *filename,
+ const int                   n_vtx,
+ const double                vtx_coord[],
+ const PDM_g_num_t           vtx_g_num[],
+ const PDM_Mesh_nodal_elt_t  elt_type,
+ const int                   n_elt,
+ const int                   elt_vtx[],
+ const PDM_g_num_t           elt_g_num[],
+ const int                   n_elt_field,
+ const char                 *elt_field_name[],
+ const double               *elt_field[]
+ );
+
 
 void
 PDM_vtk_write_ellipses
