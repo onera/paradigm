6cb5cbfef6e33ff72e2e0f3eaceef67ce0e9aee8
Bruno Maugars <bruno.maugars@onera.fr>
2021-11-12 09:03:08 +0100

    [mesh_extract] Add test file

---
+++ test/pdm_t_mesh_extract.c
@@ -0,0 +1,173 @@
+#include <math.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "pdm.h"
+#include "pdm_config.h"
+#include "pdm_mpi.h"
+#include "pdm_part.h"
+#include "pdm_dcube_gen.h"
+#include "pdm_printf.h"
+#include "pdm_error.h"
+#include "pdm_geom_elem.h"
+#include "pdm_priv.h"
+#include "pdm_predicate.h"
+
+/*============================================================================
+ * Type definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/**
+ *
+ * \brief  Usage
+ *
+ */
+
+static void
+_usage(int exit_code)
+{
+  PDM_printf
+    ("\n"
+     "  Usage: \n\n"
+     "  -n      <level>  Number of vertices on the cube side.\n\n"
+     "  -l      <level>  Cube length.\n\n"
+     "  -h               This message.\n\n");
+
+  exit(exit_code);
+}
+
+/**
+ *
+ * \brief  Read arguments from the command line
+ *
+ * \param [in]      argc     Number of arguments
+ * \param [in]      argv     Arguments
+ * \param [inout]   n_vtx_seg  Number of vertices on the cube side
+ * \param [inout]   length   Cube length
+ * \param [inout]   n_part   Number of partitions par process
+ * \param [inout]   post     Ensight outputs status
+ * \param [inout]   method   Partitioner (1 ParMETIS, 2 Pt-Scotch)
+ *
+ */
+
+static void
+_read_args(int            argc,
+           char         **argv,
+           PDM_g_num_t  *n_vtx_seg,
+           double        *length)
+{
+  int i = 1;
+
+  /* Parse and check command line */
+
+  while (i < argc) {
+
+    if (strcmp(argv[i], "-h") == 0)
+      _usage(EXIT_SUCCESS);
+
+    else if (strcmp(argv[i], "-n") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        long _n_vtx_seg = atol(argv[i]);
+        *n_vtx_seg = (PDM_g_num_t) _n_vtx_seg;
+      }
+    }
+    else if (strcmp(argv[i], "-l") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else
+        *length = atof(argv[i]);
+    }
+    else
+      _usage(EXIT_FAILURE);
+    i++;
+  }
+}
+
+
+/**
+ *
+ * \brief  Main
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+
+  PDM_g_num_t        n_vtx_seg = 10;
+  double             length    = 1.;
+
+  _read_args(argc,
+             argv,
+             &n_vtx_seg,
+             &length);
+
+  int i_rank;
+  int n_rank;
+
+  PDM_MPI_Init(&argc, &argv);
+  PDM_MPI_Comm_rank(PDM_MPI_COMM_WORLD, &i_rank);
+  PDM_MPI_Comm_size(PDM_MPI_COMM_WORLD, &n_rank);
+
+  PDM_MPI_Comm comm = PDM_MPI_COMM_WORLD;
+
+  int           dn_cell;
+  int           dn_face;
+  int           dn_vtx;
+  int           n_face_group;
+  PDM_g_num_t  *dface_cell      = NULL;
+  int          *dface_vtx_idx   = NULL;
+  PDM_g_num_t  *dface_vtx       = NULL;
+  double       *dvtx_coord      = NULL;
+  int          *dface_group_idx = NULL;
+  PDM_g_num_t  *dface_group     = NULL;
+  int           dface_vtx_l;
+  int           dface_group_l;
+
+  PDM_dcube_t* dcube = PDM_dcube_gen_init(comm,
+                                          n_vtx_seg,
+                                          length,
+                                          0.,
+                                          0.,
+                                          0.,
+                                          PDM_OWNERSHIP_KEEP);
+
+  PDM_dcube_gen_dim_get(dcube,
+                         &n_face_group,
+                         &dn_cell,
+                         &dn_face,
+                         &dn_vtx,
+                         &dface_vtx_l,
+                         &dface_group_l);
+
+  PDM_dcube_gen_data_get(dcube,
+                          &dface_cell,
+                          &dface_vtx_idx,
+                          &dface_vtx,
+                          &dvtx_coord,
+                          &dface_group_idx,
+                          &dface_group);
+
+
+
+
+
+  PDM_dcube_gen_free(dcube);
+
+  PDM_MPI_Finalize();
+
+  return 0;
+}
