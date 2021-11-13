b7e92116a71b92a5d095c6b5130fba560e0cb496
Bruno Maugars <bruno.maugars@onera.fr>
2021-11-13 06:09:01 +0100

    [connect_zone] Add test (from multipart)

---
+++ test/pdm_t_connect_zone.c
@@ -0,0 +1,290 @@
+#include <math.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "pdm.h"
+#include "pdm_config.h"
+#include "pdm_mpi.h"
+#include "pdm_part.h"
+#include "pdm_dcube_gen.h"
+#include "pdm_logging.h"
+#include "pdm_printf.h"
+#include "pdm_error.h"
+#include "pdm_priv.h"
+#include "pdm_part_to_block.h"
+#include "pdm_block_to_part.h"
+#include "pdm_distrib.h"
+#include "pdm_vtk.h"
+#include "pdm_partitioning_algorithm.h"
+#include "pdm_dconnectivity_transform.h"
+
+/*============================================================================
+ * Type definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/**
+ *
+ * \brief  Usage
+ *
+ */
+
+static void
+_usage(int exit_code)
+{
+  PDM_printf
+    ("\n"
+     "  Usage: \n\n"
+     "  -n      <level>  Number of vertices on the cube side.\n\n"
+     "  -l      <level>  Cube length.\n\n"
+     "  -h               This message.\n\n");
+
+  exit(exit_code);
+}
+
+/**
+ *
+ * \brief  Read arguments from the command line
+ *
+ * \param [in]      argc     Number of arguments
+ * \param [in]      argv     Arguments
+ * \param [inout]   n_vtx_seg  Number of vertices on the cube side
+ * \param [inout]   length   Cube length
+ * \param [inout]   n_part   Number of partitions par process
+ * \param [inout]   post     Ensight outputs status
+ * \param [inout]   method   Partitioner (1 ParMETIS, 2 Pt-Scotch)
+ *
+ */
+
+static void
+_read_args(int            argc,
+           char         **argv,
+           PDM_g_num_t  *n_vtx_seg,
+           double        *length)
+{
+  int i = 1;
+
+  /* Parse and check command line */
+
+  while (i < argc) {
+
+    if (strcmp(argv[i], "-h") == 0)
+      _usage(EXIT_SUCCESS);
+
+    else if (strcmp(argv[i], "-n") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        long _n_vtx_seg = atol(argv[i]);
+        *n_vtx_seg = (PDM_g_num_t) _n_vtx_seg;
+      }
+    }
+    else if (strcmp(argv[i], "-l") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else
+        *length = atof(argv[i]);
+    }
+    else
+      _usage(EXIT_FAILURE);
+    i++;
+  }
+}
+
+
+/**
+ *
+ * \brief  Main
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+  PDM_g_num_t        n_vtx_seg = 10;
+  double             length    = 1.;
+  int                n_zone    = 2;
+
+
+  _read_args(argc,
+             argv,
+             &n_vtx_seg,
+             &length);
+
+  int i_rank;
+  int n_rank;
+
+  PDM_MPI_Init(&argc, &argv);
+  PDM_MPI_Comm     comm = PDM_MPI_COMM_WORLD;
+  PDM_MPI_Comm_rank(comm, &i_rank);
+  PDM_MPI_Comm_size(comm, &n_rank);
+
+  /* Alloc for distributed mesh */
+  int *dn_cell       = (int *) malloc(n_zone * sizeof(int));
+  int *dn_face       = (int *) malloc(n_zone * sizeof(int));
+  int *dn_vtx        = (int *) malloc(n_zone * sizeof(int));
+  int *n_face_group  = (int *) malloc(n_zone * sizeof(int));
+  int *dface_vtx_s   = (int *) malloc(n_zone * sizeof(int));
+  int *dface_group_s = (int *) malloc(n_zone * sizeof(int));
+
+  PDM_g_num_t  **dface_cell      = (PDM_g_num_t **) malloc(n_zone * sizeof(PDM_g_num_t *));
+  int          **dface_vtx_idx   = (int         **) malloc(n_zone * sizeof(int         *));
+  PDM_g_num_t  **dface_vtx       = (PDM_g_num_t **) malloc(n_zone * sizeof(PDM_g_num_t *));
+  double       **dvtx_coord      = (double      **) malloc(n_zone * sizeof(double      *));
+  int          **dface_group_idx = (int         **) malloc(n_zone * sizeof(int         *));
+  PDM_g_num_t  **dface_group     = (PDM_g_num_t **) malloc(n_zone * sizeof(PDM_g_num_t *));
+  int          **dface_bnd_idx   = (int         **) malloc(n_zone * sizeof(int         *));
+  PDM_g_num_t  **dface_bnd       = (PDM_g_num_t **) malloc(n_zone * sizeof(PDM_g_num_t *));
+  int          **dface_join_idx  = (int         **) malloc(n_zone * sizeof(int         *));
+  PDM_g_num_t  **dface_join      = (PDM_g_num_t **) malloc(n_zone * sizeof(PDM_g_num_t *));
+  int          **djoins_ids      = (int         **) malloc(n_zone * sizeof(int         *));
+
+
+  PDM_dcube_t **dcube = (PDM_dcube_t **) malloc(n_zone * sizeof(PDM_dcube_t *));
+  for (int i_zone = 0; i_zone < n_zone; i_zone++) {
+
+    dcube[i_zone] = PDM_dcube_gen_init(comm, n_vtx_seg, length, i_zone, 0., 0., PDM_OWNERSHIP_KEEP);
+    PDM_dcube_gen_dim_get(dcube         [i_zone],
+                          &n_face_group [i_zone],
+                          &dn_cell      [i_zone],
+                          &dn_face      [i_zone],
+                          &dn_vtx       [i_zone],
+                          &dface_vtx_s  [i_zone],
+                          &dface_group_s[i_zone]);
+
+    PDM_dcube_gen_data_get(dcube          [i_zone],
+                          &dface_cell     [i_zone],
+                          &dface_vtx_idx  [i_zone],
+                          &dface_vtx      [i_zone],
+                          &dvtx_coord     [i_zone],
+                          &dface_group_idx[i_zone],
+                          &dface_group    [i_zone]);
+
+    /*
+     * Les faces groups du dcube sont : zmin, zmax, xmin, xmax, ymin, ymax
+     * Il faut les séparer en faces de bords et faces raccord, sachant que
+     * les zones sont alignées selon X
+     */
+    int n_bnd = 4;
+    int n_jn  = 2;
+    if (i_zone == 0){
+      n_bnd++;
+      n_jn-- ;
+    }
+    if (i_zone == n_zone-1){
+      n_bnd++;
+      n_jn-- ;
+    }
+
+    // Join numbering (left to right, increasing i_zone)
+    printf("n_jn = %i \n", n_jn);
+    if(n_jn > 0 ) {
+      djoins_ids[i_zone] = (int *) malloc(n_jn * sizeof(int));
+      if (i_zone == 0)
+        djoins_ids[i_zone][0] = 0;
+      else if (i_zone == n_zone-1)
+        djoins_ids[i_zone][0] = 2*i_zone - 1;
+      else {
+        djoins_ids[i_zone][0] = 2*i_zone - 1;
+        djoins_ids[i_zone][1] = 2*i_zone;
+      }
+    }
+
+    dface_bnd_idx [i_zone] = (int *) malloc((n_bnd+1) * sizeof(int));
+    dface_join_idx[i_zone] = (int *) malloc((n_jn +1) * sizeof(int));
+        // First pass to count and allocate
+    int i_bnd = 1;
+    int i_jn  = 1;
+    dface_bnd_idx[i_zone][0]  = 0;
+    dface_join_idx[i_zone][0] = 0;
+    for (int igroup = 0; igroup < n_face_group[i_zone]; igroup++) {
+      int copy_to_bnd = (igroup != 2 || (igroup == 2 && i_zone == 0)) && (igroup != 3 || (igroup == 3 && i_zone == n_zone-1));
+      int group_size = dface_group_idx[i_zone][igroup+1] - dface_group_idx[i_zone][igroup];
+      if (copy_to_bnd) { //Its a boundary
+        dface_bnd_idx[i_zone][i_bnd++] = group_size;
+      } else { //Its a join
+        dface_join_idx[i_zone][i_jn++] = group_size;
+      }
+    }
+    for (int i = 0; i < n_bnd; i++) {
+      dface_bnd_idx[i_zone][i+1] = dface_bnd_idx[i_zone][i+1] + dface_bnd_idx[i_zone][i];
+    }
+    for (int i = 0; i < n_jn; i++) {
+      dface_join_idx[i_zone][i+1] = dface_join_idx[i_zone][i+1] + dface_join_idx[i_zone][i];
+    }
+
+    // Second pass to copy
+    dface_bnd [i_zone] = (PDM_g_num_t *) malloc(dface_bnd_idx [i_zone][n_bnd] * sizeof(PDM_g_num_t));
+    dface_join[i_zone] = (PDM_g_num_t *) malloc(dface_join_idx[i_zone][n_jn ] * sizeof(PDM_g_num_t));
+    i_bnd = 0;
+    i_jn  = 0;
+    for (int igroup = 0; igroup < n_face_group[i_zone]; igroup++) {
+      int copy_to_bnd = (igroup != 2 || (igroup == 2 && i_zone == 0)) && (igroup != 3 || (igroup == 3 && i_zone == n_zone-1));
+      if (copy_to_bnd){ //Its a boundary
+        for (int i = dface_group_idx[i_zone][igroup]; i < dface_group_idx[i_zone][igroup+1]; i++) {
+          dface_bnd[i_zone][i_bnd++] = dface_group[i_zone][i];
+        }
+      } else { //Its a join
+        for (int i = dface_group_idx[i_zone][igroup]; i < dface_group_idx[i_zone][igroup+1]; i++) {
+          dface_join[i_zone][i_jn++] = dface_group[i_zone][i];
+        }
+      }
+    }
+  }
+
+  int n_total_joins = 2*(n_zone-1);
+  int *join_to_opposite = (int *) malloc( n_total_joins * sizeof(int));
+  for (int ijoin = 0; ijoin < n_total_joins; ijoin++) {
+    if (ijoin % 2 == 0) {
+      join_to_opposite[ijoin] = ijoin + 1;
+    } else {
+      join_to_opposite[ijoin] = ijoin - 1;
+    }
+  }
+
+
+  /* Free memory */
+  for (int i_zone = 0; i_zone < n_zone; i_zone++) {
+    free(dface_bnd_idx[i_zone]);
+    free(dface_bnd[i_zone]);
+    free(dface_join_idx[i_zone]);
+    free(dface_join[i_zone]);
+    if(n_zone > 1) {
+      free(djoins_ids[i_zone]);
+    }
+    PDM_dcube_gen_free(dcube[i_zone]);
+  }
+  free(dcube);
+  free(dn_cell);
+  free(dn_face);
+  free(dn_vtx);
+  free(n_face_group);
+  free(dface_group_s);
+  free(dface_vtx_s);
+  free(dface_cell);
+  free(dface_vtx_idx);
+  free(dface_vtx);
+  free(dvtx_coord);
+  free(dface_group_idx);
+  free(dface_group);
+  free(dface_bnd_idx);
+  free(dface_bnd);
+  free(dface_join_idx);
+  free(dface_join);
+  free(djoins_ids);
+  free(join_to_opposite);
+
+  PDM_MPI_Finalize();
+
+  return 0;
+}
