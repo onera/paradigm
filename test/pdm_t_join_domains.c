53e1c5bee2305b6257abb0fed1f5fc9fe5380bd8
Bastien Andrieu <bastien.andrieu@onera.fr>
2022-02-08 16:19:21 +0100

    add test

---
+++ test/pdm_t_join_domains.c
@@ -0,0 +1,332 @@
+#include <math.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "pdm.h"
+#include "pdm_config.h"
+#include "pdm_mpi.h"
+#include "pdm_part.h"
+#include "pdm_printf.h"
+#include "pdm_error.h"
+
+#include "pdm_dcube_nodal_gen.h"
+#include "pdm_domain_interface.h"
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/**
+ *
+ * \brief  Usage
+ *
+ */
+
+static void
+_usage(int exit_code)
+{
+  PDM_printf
+    ("\n"
+     "  Usage: \n\n"
+     "  -n      <level>  Number of vertices on the cube side.\n\n"
+     "  -l      <level>  Cube length.\n\n"
+     "  -n_part <level>  Number of partitions par process.\n\n"
+     "  -post            Ensight outputs (only if n_part == 1). \n\n"
+     "  -parmetis        Call ParMETIS.\n\n"
+     "  -pt-scocth       Call PT-Scotch.\n\n"
+     "  -h               This message.\n\n");
+
+  exit(exit_code);
+}
+
+
+/**
+ *
+ * \brief  Read arguments from the command line
+ *
+ * \param [in]      argc     Number of arguments
+ * \param [in]      argv     Arguments
+ * \param [inout]   n_vtx_seg  Number of vertices on the cube side
+ * \param [inout]   length   Cube length
+ * \param [inout]   n_part   Number of partitions par process
+ * \param [inout]   post     Ensight outputs status
+ * \param [inout]   method   Partitioner (1 ParMETIS, 2 Pt-Scotch)
+ *
+ */
+
+static void
+_read_args(int            argc,
+           char         **argv,
+           PDM_g_num_t   *nx,
+           PDM_g_num_t   *ny,
+           PDM_g_num_t   *nz,
+           int           *n_dom_i,
+           int           *n_dom_j,
+           int           *n_dom_k,
+           int           *periodic_i,
+           int           *periodic_j,
+           int           *periodic_k,
+           int           *t_elt,
+           double        *length,
+           int           *n_part,
+           int           *post,
+           int           *method)
+{
+  int i = 1;
+
+  /* Parse and check command line */
+
+  while (i < argc) {
+
+    if (strcmp(argv[i], "-h") == 0)
+      _usage(EXIT_SUCCESS);
+
+    else if (strcmp(argv[i], "-n") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        long _n = atol(argv[i]);
+        *nx = (PDM_g_num_t) _n;
+        *ny = (PDM_g_num_t) _n;
+        *nz = (PDM_g_num_t) _n;
+      }
+    }
+    else if (strcmp(argv[i], "-nx") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        long _n = atol(argv[i]);
+        *nx = (PDM_g_num_t) _n;
+      }
+    }
+    else if (strcmp(argv[i], "-ny") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        long _n = atol(argv[i]);
+        *ny = (PDM_g_num_t) _n;
+      }
+    }
+    else if (strcmp(argv[i], "-nz") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        long _n = atol(argv[i]);
+        *nz = (PDM_g_num_t) _n;
+      }
+    }
+    else if (strcmp(argv[i], "-ni") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        *n_dom_i = atoi(argv[i]);
+      }
+    }
+    else if (strcmp(argv[i], "-nj") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        *n_dom_j = atoi(argv[i]);
+      }
+    }
+    else if (strcmp(argv[i], "-nk") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        *n_dom_k = atoi(argv[i]);
+      }
+    }
+    else if (strcmp(argv[i], "-pi") == 0) {
+      *periodic_i = 1;
+    }
+    else if (strcmp(argv[i], "-pj") == 0) {
+      *periodic_j = 1;
+    }
+    else if (strcmp(argv[i], "-pk") == 0) {
+      *periodic_k = 1;
+    }
+    else if (strcmp(argv[i], "-t") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        *t_elt = atoi(argv[i]);
+      }
+    }
+    else if (strcmp(argv[i], "-l") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else
+        *length = atof(argv[i]);
+    }
+    else if (strcmp(argv[i], "-n_part") == 0) {
+      i++;
+      if (i >= argc)
+        _usage(EXIT_FAILURE);
+      else {
+        *n_part = atoi(argv[i]);
+      }
+    }
+    else if (strcmp(argv[i], "-post") == 0) {
+      *post = 1;
+    }
+    else if (strcmp(argv[i], "-pt-scotch") == 0) {
+      *method = 2;
+    }
+    else if (strcmp(argv[i], "-parmetis") == 0) {
+      *method = 1;
+    }
+    else
+      _usage(EXIT_FAILURE);
+    i++;
+  }
+}
+
+
+
+/**
+ *
+ * \brief  Main
+ *
+ */
+
+int main
+(
+ int   argc,
+ char *argv[]
+ )
+{
+  /*
+   *  Set default values
+   */
+  PDM_g_num_t          nx         = 10;
+  PDM_g_num_t          ny         = 10;
+  PDM_g_num_t          nz         = 10;
+  int                  n_dom_i    = 1;
+  int                  n_dom_j    = 1;
+  int                  n_dom_k    = 1;
+  int                  periodic_i = 1;
+  int                  periodic_j = 1;
+  int                  periodic_k = 1;
+  double               length     = 1.;
+  int                  n_part     = 1;
+  int                  post       = 0;
+  PDM_Mesh_nodal_elt_t t_elt      = PDM_MESH_NODAL_TRIA3;
+  // 2 -> tria
+  // 3 -> quad
+  // 5 -> tetra
+  // 6 -> pyramid
+  // 7 -> prism
+  // 8 -> hexa
+
+#ifdef PDM_HAVE_PARMETIS
+  PDM_part_split_t method  = PDM_PART_SPLIT_PARMETIS;
+#else
+#ifdef PDM_HAVE_PTSCOTCH
+  PDM_part_split_t method  = PDM_PART_SPLIT_PTSCOTCH;
+#endif
+#endif
+
+  /*
+   *  Read args
+   */
+
+  _read_args(argc,
+             argv,
+             &nx,
+             &ny,
+             &nz,
+             &n_dom_i,
+             &n_dom_j,
+             &n_dom_k,
+             &periodic_i,
+             &periodic_j,
+             &periodic_k,
+             (int *) &t_elt,
+             &length,
+             &n_part,
+             &post,
+             (int *) &method);
+
+  /*
+   *  Init
+   */
+  PDM_MPI_Comm comm = PDM_MPI_COMM_WORLD;
+  int i_rank;
+  int n_rank;
+
+  PDM_MPI_Init(&argc, &argv);
+  PDM_MPI_Comm_rank(comm, &i_rank);
+  PDM_MPI_Comm_size(comm, &n_rank);
+
+
+
+  /*
+   *
+   */
+  int n_interface = n_dom_i-1 + n_dom_j-1 + n_dom_k-1 + periodic_i + periodic_j + periodic_k;
+  int n_domain = n_dom_i * n_dom_j * n_dom_k;
+
+  PDM_dcube_nodal_t **dcube = (PDM_dcube_nodal_t **) malloc(sizeof(PDM_dcube_nodal_t *) * n_domain);
+  PDM_dmesh_nodal_t **dmn   = (PDM_dmesh_nodal_t **) malloc(sizeof(PDM_dmesh_nodal_t *) * n_domain);
+
+  const int order = 1;
+
+  for (int i = 0; i < n_domain; i++) {
+    dcube[i] = PDM_dcube_nodal_gen_create(comm,
+                                          nx,
+                                          ny,
+                                          nz,
+                                          length,
+                                          0.,
+                                          0.,
+                                          0.,
+                                          t_elt,
+                                          order,
+                                          PDM_OWNERSHIP_KEEP);
+
+    PDM_dcube_nodal_gen_build (dcube[i]);
+
+    dmn[i] = PDM_dcube_nodal_gen_dmesh_nodal_get(dcube[i]);
+    PDM_dmesh_nodal_generate_distribution(dmn[i]);
+  }
+
+  PDM_domain_interface_t *dit = PDM_domain_interface_create(n_interface,
+                                                            n_domain,
+                                                            PDM_DOMAIN_INTERFACE_MULT_NO,
+                                                            PDM_OWNERSHIP_KEEP,
+                                                            comm);
+
+
+
+
+  /*
+   *  Free memory
+   */
+  for (int i = 0; i < n_domain; i++) {
+    PDM_dcube_nodal_gen_free(dcube[i]);
+    // PDM_dcube_nodal_gen_free(dmn[i]);
+  }
+
+  PDM_domain_interface_free(dit);
+  free(dcube);
+  free(dmn);
+
+  PDM_MPI_Finalize();
+
+  return 0;
+}
