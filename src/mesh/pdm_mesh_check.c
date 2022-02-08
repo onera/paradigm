3b61a7392ddd5aaa8b64c0d95126651aa46170ae
Bastien Andrieu <bastien.andrieu@onera.fr>
2022-02-08 10:47:57 +0100

    fix pdm_t_mesh_check + Fortran interface + C & Fortran unit tests

--- src/mesh/pdm_mesh_check.c
+++ src/mesh/pdm_mesh_check.c
@@ -37,6 +37,7 @@
 #include "pdm_config.h"
 #include "pdm_printf.h"
 #include "pdm_error.h"
+#include "pdm_logging.h"
 
 /*----------------------------------------------------------------------------
  *  Header for the current file
@@ -94,17 +95,25 @@ PDM_g_num_t  nb_vtx_last_problem
   PDM_g_num_t nb_som_problem = 0 ;
 
   // Allocation
-  check_som = (PDM_g_num_t*) calloc((*n_vtx +1), sizeof(PDM_g_num_t));
+  check_som = (PDM_g_num_t*) calloc((*n_vtx), sizeof(PDM_g_num_t));
 
   // check_som à 1 pour tous les sommets cités dans la connectivité face->som
-  for (PDM_g_num_t i=1; i < *l_face_vtx; i++) {
-    index_courant = face_vtx[i] ;
+  for (PDM_g_num_t i=0; i < *l_face_vtx; i++) {
+    index_courant = face_vtx[i] - 1;
     check_som[index_courant] = 1 ;
   }
 
+  // fix trailing zeros in first pass
+  PDM_g_num_t _nb_vtx_last_problem = nb_vtx_last_problem;
+  if (nb_vtx_last_problem == *n_vtx) {
+    while (check_som[_nb_vtx_last_problem-1] == 0 && _nb_vtx_last_problem > 0) {
+      _nb_vtx_last_problem--;
+    }
+  }
+
   // si check_som est 0 quelquepart, c'est qu'il y a un trou dans la connectivité
-  for (PDM_g_num_t i=nb_vtx_last_problem; i > 0; i--) {
-    if (check_som[i] == 0) {
+  for (PDM_g_num_t i=_nb_vtx_last_problem; i > 0; i--) {
+    if (check_som[i-1] == 0) {
       nb_som_problem = i ;
       break ;
     }
@@ -134,8 +143,8 @@ static void
 _unconnected_vertex_remove
 (
 PDM_g_num_t* n_vtx,
-PDM_g_num_t nb_vtx_problem,
-double* coords,
+PDM_g_num_t  nb_vtx_problem,
+double*      coords,
 PDM_g_num_t* l_face_vtx,
 PDM_g_num_t* face_vtx
 )
@@ -145,13 +154,13 @@ PDM_g_num_t* face_vtx
 
   // Coordonnees
   for (PDM_g_num_t i=nb_vtx_problem; i < *n_vtx; i++) {
-    coords[3*(i-1)] = coords[3*(i-1)+3] ; //x(n-1) <- x(n)
-    coords[3*(i-1)+1] = coords[3*(i-1)+1+3] ; //y(n-1) <- y(n)
-    coords[3*(i-1)+2] = coords[3*(i-1)+2+3] ; //z(n-1) <- z(n)
+    coords[3*(i-1)  ] = coords[3*i  ] ; //x(n-1) <- x(n)
+    coords[3*(i-1)+1] = coords[3*i+1] ; //y(n-1) <- y(n)
+    coords[3*(i-1)+2] = coords[3*i+2] ; //z(n-1) <- z(n)
   }
 
   // Connectivité faces->sommets
-  for (PDM_g_num_t i=1; i < *l_face_vtx; i++) {
+  for (PDM_g_num_t i=0; i < *l_face_vtx; i++) {
     index_courant = face_vtx[i] ;
     if (index_courant > nb_vtx_problem) {
       face_vtx[i] = index_courant - 1 ;
@@ -169,7 +178,7 @@ PDM_g_num_t* face_vtx
  * If a vertex is not cited in the face->vtx connectivity, the function
  * removes it from the mesh to ensure contiguity
  *
- * /TODO : PDM_Mesh_check_unconnected_vertex complexity is n^2. This function must be optimized
+ * /TODO : PDM_mesh_check_unconnected_vertex complexity is n^2. This function must be optimized
  *
  * \param [in, out] n_vtx       Number of vertices
  * \param [in, out] l_face_vtx  Size of face->vtx connectivity
@@ -179,31 +188,13 @@ PDM_g_num_t* face_vtx
  *
  */
 
-void PROCF (pdm_mesh_check_unconnected_vertex, PDM_MESH_CHECK_UNCONNECTED_VERTEX)
-(
-PDM_g_num_t *nb_vtx,
-PDM_g_num_t *l_face_vtx,
-PDM_g_num_t *face_vtx,
-double      *coords,
-int         *nb_holes
-)
-{
-
-  PDM_Mesh_check_unconnected_vertex(nb_vtx,
-                     l_face_vtx,
-                     face_vtx,
-                     coords,
-                     nb_holes);
-}
-
-
-void PDM_Mesh_check_unconnected_vertex
+void PDM_mesh_check_unconnected_vertex
 (
 PDM_g_num_t* nb_vtx,
-PDM_g_num_t* face_vtx,
 PDM_g_num_t* l_face_vtx,
-double* coords,
-int* nb_holes
+PDM_g_num_t* face_vtx,
+double*      coords,
+int*         nb_holes
 )
 {
 
