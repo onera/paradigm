9b39de380fdb3459e240b6a068c82b61cf573d77
Karmijn Hoogveld <karmijn.hoogveld@onera.fr>
2022-06-17 15:01:28 +0200

    3d derivative uvw

--- src/mesh/pdm_ho_bezier_basis.c
+++ src/mesh/pdm_ho_bezier_basis.c
@@ -266,93 +266,197 @@ _basis_bezier_edge_derivative
  *
  * \brief Triangle Bézier basis derivative
  *
+ * \param [in]  mesh_dimension  Mesh dimension
  * \param [in]  order           Order
  * \param [in]  n_pts           Number of points
  * \param [in]  uv              Parametric coordinates (size = \ref n_pts)
  * \param [out] dw_du           Weights derivative with respect to u (size = n_nodes * \ref n_pts)
  * \param [out] dw_dv           Weights derivative with respect to v (size = n_nodes * \ref n_pts)
+ * \param [out] dw_dw           Weights derivative with respect to w = 1 - u - v (size = n_nodes * \ref n_pts)
  *
  */
 
 static void
 _basis_bezier_tria_derivative
 (
+ const int              mesh_dimension,
  const int              order,
  const int              n_pts,
  const double *restrict uv,
  double       *restrict dw_du,
- double       *restrict dw_dv
+ double       *restrict dw_dv,
+ double       *restrict dw_dw
 )
 {
+  if (mesh_dimension == 2) {
 
-  if (order == 1) {
-    for (int i = 0; i < n_pts; i++) {
+    if (order == 1) {
+      for (int i = 0; i < n_pts; i++) {
 
-      dw_du[3*i]   = -1;
-      dw_du[3*i+1] = 1;
-      dw_du[3*i+2] = 0;
+        dw_du[3*i]   = -1;
+        dw_du[3*i+1] = 1;
+        dw_du[3*i+2] = 0;
 
-      dw_dv[3*i]   = -1;
-      dw_dv[3*i+1] = 0;
-      dw_dv[3*i+2] = 1;
+        dw_dv[3*i]   = -1;
+        dw_dv[3*i+1] = 0;
+        dw_dv[3*i+2] = 1;
 
+      }
     }
-  }
 
-  if (order == 2) {
-    for (int i = 0; i < n_pts; i++) {
+    if (order == 2) {
+      for (int i = 0; i < n_pts; i++) {
 
-      double u =  uv[2*i];
-      double v =  uv[2*i+1];
+        double u =  uv[2*i];
+        double v =  uv[2*i+1];
 
-      dw_du[6*i+0] = 2 * (-1 + u + v);    // (i,j,k)=(0,0,2)
-      dw_du[6*i+1] = 2 * (1 - 2 * u - v); // (i,j,k)=(1,0,1)
-      dw_du[6*i+2] = 2 * u;               // (i,j,k)=(2,0,0)
-      dw_du[6*i+3] = 2 * (- v);           // (i,j,k)=(0,1,1)
-      dw_du[6*i+4] = 2 * v;               // (i,j,k)=(1,1,0)
-      dw_du[6*i+5] = 0;                   // (i,j,k)=(0,2,0)
+        dw_du[6*i+0] = 2 * (-1 + u + v);    // (i,j,k)=(0,0,2)
+        dw_du[6*i+1] = 2 * (1 - 2 * u - v); // (i,j,k)=(1,0,1)
+        dw_du[6*i+2] = 2 * u;               // (i,j,k)=(2,0,0)
+        dw_du[6*i+3] = 2 * (- v);           // (i,j,k)=(0,1,1)
+        dw_du[6*i+4] = 2 * v;               // (i,j,k)=(1,1,0)
+        dw_du[6*i+5] = 0;                   // (i,j,k)=(0,2,0)
 
-      dw_dv[6*i+0] = 2 * (-1 + v + u);    // (i,j,k)=(0,0,2)
-      dw_dv[6*i+1] = 2 * (- u);           // (i,j,k)=(1,0,1)
-      dw_dv[6*i+2] = 0;                   // (i,j,k)=(2,0,0)
-      dw_dv[6*i+3] = 2 * (1 - u - 2 * v); // (i,j,k)=(0,1,1)
-      dw_dv[6*i+4] = 2 * u;               // (i,j,k)=(1,1,0)
-      dw_dv[6*i+5] = 2 * v;               // (i,j,k)=(0,2,0)
+        dw_dv[6*i+0] = 2 * (-1 + v + u);    // (i,j,k)=(0,0,2)
+        dw_dv[6*i+1] = 2 * (- u);           // (i,j,k)=(1,0,1)
+        dw_dv[6*i+2] = 0;                   // (i,j,k)=(2,0,0)
+        dw_dv[6*i+3] = 2 * (1 - u - 2 * v); // (i,j,k)=(0,1,1)
+        dw_dv[6*i+4] = 2 * u;               // (i,j,k)=(1,1,0)
+        dw_dv[6*i+5] = 2 * v;               // (i,j,k)=(0,2,0)
 
+      }
     }
-  }
 
-  if (order == 3) {
+    if (order == 3) {
 
-    for (int i = 0; i < n_pts; i++) {
+      for (int i = 0; i < n_pts; i++) {
 
-      double u =  uv[2*i];
-      double v =  uv[2*i+1];
+        double u =  uv[2*i];
+        double v =  uv[2*i+1];
 
-      dw_du[10*i+0] = 3 * (- u * u - 1 + 2 * v + 2 * u - v * v - 2 * u * v);   // (i,j,k)=(003)
-      dw_du[10*i+3] = 3 * u * u;                                               // (i,j,k)=(300)
-      dw_du[10*i+9] = 0;                                                       // (i,j,k)=(030)
-      dw_du[10*i+1] = (1 - 4 * u - 2 * v + 4 * u * v + 3 * u * u + v * v);     // (i,j,k)=(102)
-      dw_du[10*i+2] = 3 * (2 * u - 3 * u * u - 2 * u * v);                     // (i,j,k)=(201)
-      dw_du[10*i+6] = 6 * u * v;                                               // (i,j,k)=(210)
-      dw_du[10*i+8] = 3 * v * v;                                               // (i,j,k)=(120)
-      dw_du[10*i+7] = -3 * v * v;                                              // (i,j,k)=(021)
-      dw_du[10*i+4] = 3 * (1 - 2 * u - 2 * v * v + 2 * u * v);                 // (i,j,k)=(012)
-      dw_du[10*i+5] = 6 * (v - 2 * u * v - v * v);                             // (i,j,k)=(111)
+        dw_du[10*i+0] = 3 * (- u * u - 1 + 2 * v + 2 * u - v * v - 2 * u * v);   // (i,j,k)=(003)
+        dw_du[10*i+3] = 3 * u * u;                                               // (i,j,k)=(300)
+        dw_du[10*i+9] = 0;                                                       // (i,j,k)=(030)
+        dw_du[10*i+1] = (1 - 4 * u - 2 * v + 4 * u * v + 3 * u * u + v * v);     // (i,j,k)=(102)
+        dw_du[10*i+2] = 3 * (2 * u - 3 * u * u - 2 * u * v);                     // (i,j,k)=(201)
+        dw_du[10*i+6] = 6 * u * v;                                               // (i,j,k)=(210)
+        dw_du[10*i+8] = 3 * v * v;                                               // (i,j,k)=(120)
+        dw_du[10*i+7] = -3 * v * v;                                              // (i,j,k)=(021)
+        dw_du[10*i+4] = 3 * (1 - 2 * u - 2 * v * v + 2 * u * v);                 // (i,j,k)=(012)
+        dw_du[10*i+5] = 6 * (v - 2 * u * v - v * v);                             // (i,j,k)=(111)
 
-      dw_dv[10*i+0] = 3 * (- v * v - 1 + 2 * u + 2 * v - 2 * u * v - u * u);   // (i,j,k)=(003)
-      dw_dv[10*i+3] = 0;                                                       // (i,j,k)=(300)
-      dw_dv[10*i+9] = 3 * v * v;                                               // (i,j,k)=(030)
-      dw_dv[10*i+1] = 3 * (-2 * u + 2 * u * u + 2 * v * u);                    // (i,j,k)=(102)
-      dw_dv[10*i+2] = -3 * u * u;                                              // (i,j,k)=(201)
-      dw_dv[10*i+6] = 3 * u * u;                                               // (i,j,k)=(210)
-      dw_dv[10*i+8] = 6 * u * v;                                               // (i,j,k)=(120)
-      dw_dv[10*i+7] = 3 * (2* v - 2 * u * v - 3 * v * v);                      // (i,j,k)=(021)
-      dw_dv[10*i+4] = 3 * (1 - 2 * u - 4 * v + 4 * u * v + u * u + 3 * v * v); // (i,j,k)=(012)
-      dw_dv[10*i+5] = 6 * (u - u * u - 2 * u * v);                             // (i,j,k)=(111)
+        dw_dv[10*i+0] = 3 * (- v * v - 1 + 2 * u + 2 * v - 2 * u * v - u * u);   // (i,j,k)=(003)
+        dw_dv[10*i+3] = 0;                                                       // (i,j,k)=(300)
+        dw_dv[10*i+9] = 3 * v * v;                                               // (i,j,k)=(030)
+        dw_dv[10*i+1] = 3 * (-2 * u + 2 * u * u + 2 * v * u);                    // (i,j,k)=(102)
+        dw_dv[10*i+2] = -3 * u * u;                                              // (i,j,k)=(201)
+        dw_dv[10*i+6] = 3 * u * u;                                               // (i,j,k)=(210)
+        dw_dv[10*i+8] = 6 * u * v;                                               // (i,j,k)=(120)
+        dw_dv[10*i+7] = 3 * (2* v - 2 * u * v - 3 * v * v);                      // (i,j,k)=(021)
+        dw_dv[10*i+4] = 3 * (1 - 2 * u - 4 * v + 4 * u * v + u * u + 3 * v * v); // (i,j,k)=(012)
+        dw_dv[10*i+5] = 6 * (u - u * u - 2 * u * v);                             // (i,j,k)=(111)
 
+      }
     }
-  }
+
+  } // en 2D case
+
+  if (mesh_dimension == 3) {
+
+    if (order == 1) {
+      for (int i = 0; i < n_pts; i++) {
+
+        dw_du[3*i]   = 0;
+        dw_du[3*i+1] = 1;
+        dw_du[3*i+2] = 0;
+
+        dw_dv[3*i]   = 0;
+        dw_dv[3*i+1] = 0;
+        dw_dv[3*i+2] = 1;
+
+        dw_dw[3*i]   = 1;
+        dw_dw[3*i+1] = 0;
+        dw_dw[3*i+2] = 0;
+
+      }
+    }
+
+    if (order == 2) {
+      for (int i = 0; i < n_pts; i++) {
+
+        double u =  uv[2*i];
+        double v =  uv[2*i+1];
+
+        dw_du[6*i+0] = 0;              // (i,j,k)=(0,0,2)
+        dw_du[6*i+1] = 2 * (1 - u -v); // (i,j,k)=(1,0,1)
+        dw_du[6*i+2] = 2 * u;          // (i,j,k)=(2,0,0)
+        dw_du[6*i+3] = 0;              // (i,j,k)=(0,1,1)
+        dw_du[6*i+4] = 2 * v;          // (i,j,k)=(1,1,0)
+        dw_du[6*i+5] = 0;              // (i,j,k)=(0,2,0)
+
+        dw_dv[6*i+0] = 0;              // (i,j,k)=(0,0,2)
+        dw_dv[6*i+1] = 0;              // (i,j,k)=(1,0,1)
+        dw_dv[6*i+2] = 0;              // (i,j,k)=(2,0,0)
+        dw_dv[6*i+3] = 2 * (1 - u -v); // (i,j,k)=(0,1,1)
+        dw_dv[6*i+4] = 2 * u;          // (i,j,k)=(1,1,0)
+        dw_dv[6*i+5] = 2 * v;          // (i,j,k)=(0,2,0)
+
+        dw_dv[6*i+0] = 2 * (1 - u -v); // (i,j,k)=(0,0,2)
+        dw_dv[6*i+1] = 2 * u;          // (i,j,k)=(1,0,1)
+        dw_dv[6*i+2] = 0;              // (i,j,k)=(2,0,0)
+        dw_dv[6*i+3] = 2 * v;          // (i,j,k)=(0,1,1)
+        dw_dv[6*i+4] = 0;              // (i,j,k)=(1,1,0)
+        dw_dv[6*i+5] = 0;              // (i,j,k)=(0,2,0)
+
+      }
+    }
+
+    if (order == 3) {
+
+      for (int i = 0; i < n_pts; i++) {
+
+        double u =  uv[2*i];
+        double v =  uv[2*i+1];
+
+        dw_du[10*i+0] =  0;                           // (i,j,k)=(003)
+        dw_du[10*i+3] =  3 * u * u;                   // (i,j,k)=(300)
+        dw_du[10*i+9] =  0;                           // (i,j,k)=(030)
+        dw_du[10*i+1] =  3 * (1 - u -v) * (1 - u -v); // (i,j,k)=(102)
+        dw_du[10*i+2] =  6 * u * (1 -u -v);           // (i,j,k)=(201)
+        dw_du[10*i+6] =  6 * u * v;                   // (i,j,k)=(210)
+        dw_du[10*i+8] =  3 *  v * v;                  // (i,j,k)=(120)
+        dw_du[10*i+7] =  0;                           // (i,j,k)=(021)
+        dw_du[10*i+4] =  0;                           // (i,j,k)=(012)
+        dw_du[10*i+5] =  6 * v * (1 - u -v);          // (i,j,k)=(111)
+
+        dw_dv[10*i+0] =  0;                           // (i,j,k)=(003)
+        dw_dv[10*i+3] =  0;                           // (i,j,k)=(300)
+        dw_dv[10*i+9] =  3 * v * v;                   // (i,j,k)=(030)
+        dw_dv[10*i+1] =  0;                           // (i,j,k)=(102)
+        dw_dv[10*i+2] =  0;                           // (i,j,k)=(201)
+        dw_dv[10*i+6] =  3 * u * u;                   // (i,j,k)=(210)
+        dw_dv[10*i+8] =  6 * u * v;                   // (i,j,k)=(120)
+        dw_dv[10*i+7] =  6 * v * (1 - u -v);          // (i,j,k)=(021)
+        dw_dv[10*i+4] =  3 * (1 - u -v) * (1 - u -v); // (i,j,k)=(012)
+        dw_dv[10*i+5] =  6 * u * (1 - u -v);          // (i,j,k)=(111)
+
+        dw_dv[10*i+0] =  3 * (1 - u -v) * (1 - u -v); // (i,j,k)=(003)
+        dw_dv[10*i+3] =  0;                           // (i,j,k)=(300)
+        dw_dv[10*i+9] =  0;                           // (i,j,k)=(030)
+        dw_dv[10*i+1] =  6 * u * (1 - u -v);          // (i,j,k)=(102)
+        dw_dv[10*i+2] =  3 * u * u;                   // (i,j,k)=(201)
+        dw_dv[10*i+6] =  0;                           // (i,j,k)=(210)
+        dw_dv[10*i+8] =  0;                           // (i,j,k)=(120)
+        dw_dv[10*i+7] =  3 * v * v;                   // (i,j,k)=(021)
+        dw_dv[10*i+4] =  6 * v * (1 - u -v);          // (i,j,k)=(012)
+        dw_dv[10*i+5] =  6 * u * v;                   // (i,j,k)=(111)
+
+      }
+    }
+
+  } // en 3D case
+
+
 
   if (order > 3) {
     PDM_error(__FILE__, __LINE__, 0, "Not implemented yet for order > 3\n");
--- test/pdm_t_ho_bezier_intersect.c
+++ test/pdm_t_ho_bezier_intersect.c
@@ -30,27 +30,88 @@
 int main(int argc, char *argv[])
 {
 
-  // Do triangle line intersection
+  // Set-up test
+  double *line       = malloc(sizeof(double) * 6);
+  double *tria_coord = malloc(sizeof(double) * 9);
+  double *ip         = malloc(sizeof(double) * 3);
+
+  line[0] = 0; line[3] = 0;
+  line[1] = 0; line[4] = 0;
+  line[2] = 0; line[5] = 0;
+
+  tria_coord[0] = 0; tria_coord[3] = 0; tria_coord[6] = 0;
+  tria_coord[1] = 0; tria_coord[4] = 0; tria_coord[7] = 0;
+  tria_coord[2] = 0; tria_coord[5] = 0; tria_coord[8] = 0;
+
+  // Do triangle P1 line intersection
 
   PDM_triangle_line_intersection(line, tria_coord, ip);
 
-  // Get xyz coordinates from uv coordinates
+  // Construct matrices.  Since we have over determined system, need to find
+  // which 2 out of 3 equations to use to develop equations. (Any 2 should
+  // work since we've projected point to plane.)
 
+  double rhs[2], c1[2], c2[2];
+
+  for (int i = 0; i < 2; i++) {
+    rhs[i] = ip[i] - tria_coord[6 + i];
+    c1[i] = tria_coord[i] - tria_coord[6 + i];
+    c2[i] = tria_coord[3 + i] - tria_coord[6 + i];
+  }
+
+  double det = PDM_DETERMINANT2X2(c1,c2);
+
+  double pcoords[3];
+
+  pcoords[0] = PDM_DETERMINANT2X2(rhs,c2) / det;
+  pcoords[1] = PDM_DETERMINANT2X2(c1,rhs) / det;
+
+  double weights[3];
+  double closest_point[3];
+
+  weights[0] = 1 - (pcoords[0] + pcoords[1]);
+  weights[1] = pcoords[0];
+  weights[2] = pcoords[1];
+  if ( weights[0] >= 0.0 && weights[0] <= 1.0 &&
+       weights[1] >= 0.0 && weights[1] <= 1.0 &&
+       weights[2] >= 0.0 && weights[2] <= 1.0 ) {
+
+    // Projection distance
+
+    closest_point[0] = ip[0];
+    closest_point[1] = ip[1];
+    closest_point[2] = ip[2];
+
+  } // end is inside case
+
+  // Get uvw in triangle Pn
+
+  double uv_clossest_Pn[3];
+
+  double _uvPn_sub_tria[6];
+
+  _uvPn_sub_tria[0] = uv_nodes[2*idx1];
+  _uvPn_sub_tria[1] = uv_nodes[2*idx1+1];
+  _uvPn_sub_tria[2] = uv_nodes[2*idx2];
+  _uvPn_sub_tria[3] = uv_nodes[2*idx2+1];
+  _uvPn_sub_tria[4] = uv_nodes[2*idx3];
+  _uvPn_sub_tria[5] = uv_nodes[2*idx3+1];
+
+  for (int j = 0; j < 2; j++) {
+    for (int k = 0; k < 3; k++) {
+      uv_clossest_Pn[j] += weights[k] * uvPn_sub_tria[2*k + j];
+    }
+  }
+
+  // Get xyz coordinates from uvw coordinates
 
   double *uvw     = malloc(sizeof(double) * 3 * 9);
-  double *weights = malloc(sizeof(double) * 3 * 9);
-
-  PDM_ho_bezier_basis(PDM_MESH_NODAL_TRIA3,
-                      1,
-                      3,
-                      uvw,
-                      weigths);
-
+  double *weightsPn = malloc(sizeof(double) * 3 * 9);
 
   PDM_ho_bezier_basis(PDM_MESH_NODAL_TRIAHO,
                       2,
                       6,
-                      uvw,
-                      weigths);
+                      uv_clossest_Pn,
+                      weigthsPn);
 
 }
