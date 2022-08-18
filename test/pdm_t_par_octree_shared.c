a30835cf9b5a1fb8aecdc416571c746cecf7ff1e
Bruno Maugars <bruno.maugars@onera.fr>
2022-08-18 19:10:36 +0200

    [pdm_para_octree] Add test for shared

---
+++ test/pdm_t_par_octree_shared.c
@@ -0,0 +1,229 @@
+#include <math.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "pdm.h"
+#include "pdm_priv.h"
+
+#include "pdm_mpi.h"
+#include "pdm_config.h"
+#include "pdm_part_to_block.h"
+#include "pdm_printf.h"
+#include "pdm_error.h"
+#include "pdm_gnum.h"
+#include "pdm_point_cloud_gen.h"
+#include "pdm_para_octree.h"
+
+/*============================================================================
+ * Macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Type definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/**
+ *
+ * \brief  Usage
+ *
+ */
+
+static void
+_usage
+(
+int exit_code
+)
+{
+  PDM_printf
+    ("\n"
+     "  Usage: \n\n"
+     "  -n      <level>  Number of points (default : 10).\n\n"
+     "  -radius <level>  Radius of domain (default : 10).\n\n"
+     "  -local           Number of points is local (default : global).\n\n"
+     "  -rand            Random definition of point coordinates (default : false).\n\n"
+     "  -h               This message.\n\n");
+
+  exit (exit_code);
+}
+
+
+/**
+ *
+ * \brief  Read arguments from the command line
+ *
+ * \param [in]    argc   Number of arguments
+ * \param [in]    argv   Arguments
+ * \param [inout] nPts   Number of points
+ * \param [inout] ls     Low scalability
+ * \param [inout] length Length of domains
+ *
+ */
+
+static void
+_read_args
+(
+ int            argc,
+ char         **argv,
+ PDM_g_num_t   *nPts,
+ double        *radius,
+ int           *local,
+ int           *rand
+)
+{
+  int i = 1;
+
+  /* Parse and check command line */
+
+  while (i < argc) {
+
+    if (strcmp(argv[i], "-h") == 0)
+      _usage(EXIT_SUCCESS);
+
+    else if (strcmp(argv[i], "-n") == 0) {
+      i++;
+      if (i >= argc) {
+        _usage(EXIT_FAILURE);
+      }
+      else {
+        long _nPts = atol(argv[i]);
+        *nPts = (PDM_g_num_t) _nPts;
+      }
+    }
+
+    else if (strcmp(argv[i], "-radius") == 0) {
+      i++;
+      if (i >= argc) {
+        _usage(EXIT_FAILURE);
+      }
+      else {
+        *radius = atof(argv[i]);
+      }
+    }
+
+    else if (strcmp(argv[i], "-local") == 0) {
+      *local = 1;
+    }
+
+    else if (strcmp(argv[i], "-rand") == 0) {
+      *rand = 1;
+    }
+
+    else {
+      _usage(EXIT_FAILURE);
+    }
+    i++;
+  }
+}
+
+/*============================================================================
+ * Public function definitions
+ *============================================================================*/
+
+/**
+ *
+ * \brief  Main
+ *
+ */
+
+int
+main
+(
+int argc,
+char *argv[]
+)
+{
+
+  PDM_MPI_Init (&argc, &argv);
+
+  int i_rank;
+  PDM_MPI_Comm_rank (PDM_MPI_COMM_WORLD, &i_rank);
+
+  int numProcs;
+  PDM_MPI_Comm_size (PDM_MPI_COMM_WORLD, &numProcs);
+
+  PDM_g_num_t nPts   = 10;
+  double radius = 10.;
+  int local = 0;
+  int rand = 0;
+
+  _read_args(argc,
+             argv,
+             &nPts,
+             &radius,
+             &local,
+             &rand);
+
+  /* Initialize random */
+
+  if (rand) {
+    srand(time(NULL));
+  }
+  else {
+    srand(i_rank);
+  }
+
+  /* Random point cloud */
+  int _n_pts_l;
+  double      *coords = NULL;
+  PDM_g_num_t *gnum   = NULL;
+  PDM_point_cloud_gen_random (PDM_MPI_COMM_WORLD,
+                              nPts,
+                              -radius, -radius, -radius,
+                              radius, radius, radius,
+                              &_n_pts_l,
+                              &coords,
+                              &gnum);
+
+  /* Parallel octree */
+
+  const int n_point_cloud = 1;
+  const int depth_max = 31;
+  const int points_in_leaf_max = 1;
+
+  const int build_leaf_neighbours = 1;
+  PDM_para_octree_t *octree = PDM_para_octree_create (n_point_cloud,
+                                                      depth_max,
+                                                      points_in_leaf_max,
+                                                      build_leaf_neighbours,
+                                                      PDM_MPI_COMM_WORLD);
+
+  PDM_para_octree_point_cloud_set (octree, 0, _n_pts_l, coords, gnum);
+
+  PDM_para_octree_build (octree, NULL);
+
+  // PDM_para_octree_dump (octree);
+
+  // PDM_para_octree_build_shared(octree);
+
+  PDM_para_octree_dump_times (octree);
+
+  PDM_para_octree_free (octree);
+
+  /* Free */
+
+  free (coords);
+  free (gnum);
+
+  if (i_rank == 0) {
+    PDM_printf ("-- End\n");
+    fflush(stdout);
+  }
+
+
+  PDM_MPI_Barrier (PDM_MPI_COMM_WORLD);
+
+  PDM_MPI_Finalize ();
+
+  return 0;
+
+}
