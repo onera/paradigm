8ea4e91303ba3c9a29e34a7313c3185086708e40
Bruno Maugars <bruno.maugars@onera.fr>
2022-08-21 15:47:32 +0200

    [mpi_shared] Neighbor_in OK

--- test/pdm_t_mpi_shared_allgather.c
+++ test/pdm_t_mpi_shared_allgather.c
@@ -40,63 +40,63 @@ PDM_MPI_Comm setup_numa_graph(PDM_MPI_Comm comm)
   PDM_MPI_Comm comm_shared;
   PDM_MPI_Comm_split_type(comm, PDM_MPI_SPLIT_NUMA, &comm_shared);
 
-  int n_rank_in_node, i_rank_in_node;
-  PDM_MPI_Comm_rank (comm_shared, &i_rank_in_node);
-  PDM_MPI_Comm_size (comm_shared, &n_rank_in_node);
+  int n_rank_in_shm, i_rank_in_shm;
+  PDM_MPI_Comm_rank (comm_shared, &i_rank_in_shm);
+  PDM_MPI_Comm_size (comm_shared, &n_rank_in_shm);
 
   PDM_MPI_Comm comm_same_numa_id;
-  PDM_MPI_Comm_split(comm, i_rank_in_node /* color*/, i_rank /* key */, &comm_same_numa_id);
+  PDM_MPI_Comm_split(comm, i_rank_in_shm /* color*/, i_rank /* key */, &comm_same_numa_id);
 
   int n_rank_same_numa_id, i_rank_same_numa_id;
   PDM_MPI_Comm_rank (comm_same_numa_id, &i_rank_same_numa_id);
   PDM_MPI_Comm_size (comm_same_numa_id, &n_rank_same_numa_id);
 
   if(1 == 1) {
-    log_trace("i_rank = %i [%i] | i_rank_in_node = %i [%i] | i_rank_same_numa_id = %i [%i] \n",
-              i_rank, n_rank, i_rank_in_node, n_rank_in_node, i_rank_same_numa_id, n_rank_same_numa_id);
+    log_trace("i_rank = %i [%i] | i_rank_in_shm = %i [%i] | i_rank_same_numa_id = %i [%i] \n",
+              i_rank, n_rank, i_rank_in_shm, n_rank_in_shm, i_rank_same_numa_id, n_rank_same_numa_id);
 
   }
 
   /*
    * Prepare to save rank_id in shared memory
    */
-  PDM_mpi_win_shared_t* wsame_numa_id_core_idx = PDM_mpi_win_shared_create(n_rank_in_node+1, sizeof(int), comm_shared);
+  PDM_mpi_win_shared_t* wsame_numa_id_core_idx = PDM_mpi_win_shared_create(n_rank_in_shm+1, sizeof(int), comm_shared);
   int *same_numa_id_core_idx = PDM_mpi_win_shared_get(wsame_numa_id_core_idx);
   PDM_mpi_win_shared_lock_all (0, wsame_numa_id_core_idx);
 
-  same_numa_id_core_idx[i_rank_in_node+1] = n_rank_same_numa_id;
+  same_numa_id_core_idx[i_rank_in_shm+1] = n_rank_same_numa_id;
   PDM_mpi_win_shared_sync(wsame_numa_id_core_idx);
   PDM_MPI_Barrier(comm_shared);
 
   /*
    * Create index
    */
-  if(i_rank_in_node == 0) {
+  if(i_rank_in_shm == 0) {
     same_numa_id_core_idx[0] = 0;
-    for(int i = 0; i < n_rank_in_node; ++i) {
+    for(int i = 0; i < n_rank_in_shm; ++i) {
       same_numa_id_core_idx[i+1] += same_numa_id_core_idx[i];
     }
   }
   PDM_mpi_win_shared_sync(wsame_numa_id_core_idx);
   PDM_MPI_Barrier(comm_shared);
 
-  PDM_log_trace_array_int(same_numa_id_core_idx, n_rank_in_node+1, "wsame_numa_id_core_idx ::");
+  PDM_log_trace_array_int(same_numa_id_core_idx, n_rank_in_shm+1, "wsame_numa_id_core_idx ::");
 
   /*
    * Get rank number
    */
-  PDM_mpi_win_shared_t* wsame_numa_id_rank_gid = PDM_mpi_win_shared_create(same_numa_id_core_idx[n_rank_in_node], sizeof(int), comm_shared);
+  PDM_mpi_win_shared_t* wsame_numa_id_rank_gid = PDM_mpi_win_shared_create(same_numa_id_core_idx[n_rank_in_shm], sizeof(int), comm_shared);
   int *rank_gid = PDM_mpi_win_shared_get(wsame_numa_id_rank_gid);
   PDM_mpi_win_shared_lock_all (0, wsame_numa_id_rank_gid);
 
-  int *lrank_id = &rank_gid[same_numa_id_core_idx[i_rank_in_node]];
+  int *lrank_id = &rank_gid[same_numa_id_core_idx[i_rank_in_shm]];
 
   PDM_MPI_Allgather(&i_rank, 1, PDM_MPI_INT, lrank_id, 1, PDM_MPI_INT, comm_same_numa_id);
   PDM_mpi_win_shared_sync(wsame_numa_id_rank_gid);
   PDM_MPI_Barrier(comm_shared);
 
   if(1 == 1) {
-    PDM_log_trace_array_int(rank_gid, same_numa_id_core_idx[n_rank_in_node], "rank_gid ::");
+    PDM_log_trace_array_int(rank_gid, same_numa_id_core_idx[n_rank_in_shm], "rank_gid ::");
   }
 
   /*
@@ -115,9 +115,9 @@ PDM_MPI_Comm setup_numa_graph(PDM_MPI_Comm comm)
   /*
    * Build shared array of number of numa
    */
-  PDM_mpi_win_shared_t* wval_tot_n = PDM_mpi_win_shared_create(same_numa_id_core_idx[n_rank_in_node], sizeof(int), comm_shared);
+  PDM_mpi_win_shared_t* wval_tot_n = PDM_mpi_win_shared_create(same_numa_id_core_idx[n_rank_in_shm], sizeof(int), comm_shared);
   int *val_tot_n  = PDM_mpi_win_shared_get(wval_tot_n);
-  int *lval_tot_n = &val_tot_n[same_numa_id_core_idx[i_rank_in_node]];
+  int *lval_tot_n = &val_tot_n[same_numa_id_core_idx[i_rank_in_shm]];
   PDM_mpi_win_shared_lock_all (0, wval_tot_n);
 
 
@@ -126,7 +126,7 @@ PDM_MPI_Comm setup_numa_graph(PDM_MPI_Comm comm)
   PDM_MPI_Barrier(comm_shared);
 
   if(1 == 1) {
-    PDM_log_trace_array_int(val_tot_n, same_numa_id_core_idx[n_rank_in_node], "val_tot_n ::");
+    PDM_log_trace_array_int(val_tot_n, same_numa_id_core_idx[n_rank_in_shm], "val_tot_n ::");
   }
 
   /*
@@ -180,35 +180,35 @@ PDM_MPI_Comm setup_numa_graph2(PDM_MPI_Comm comm)
   PDM_MPI_Comm comm_shared;
   PDM_MPI_Comm_split_type(comm, PDM_MPI_SPLIT_NUMA, &comm_shared);
 
-  int n_rank_in_node, i_rank_in_node;
-  PDM_MPI_Comm_rank (comm_shared, &i_rank_in_node);
-  PDM_MPI_Comm_size (comm_shared, &n_rank_in_node);
+  int n_rank_in_shm, i_rank_in_shm;
+  PDM_MPI_Comm_rank (comm_shared, &i_rank_in_shm);
+  PDM_MPI_Comm_size (comm_shared, &n_rank_in_shm);
 
-  PDM_MPI_Comm comm_master_of_shared = PDM_MPI_get_group_of_master(comm, comm_shared);
+  PDM_MPI_Comm comm_master_of_shm = PDM_MPI_get_group_of_master(comm, comm_shared);
 
-  int i_rank_master_of_shared = -1;
-  int n_rank_master_of_shared;
-  if(comm_master_of_shared != PDM_MPI_COMM_NULL) {
-    PDM_MPI_Comm_rank(comm_master_of_shared, &i_rank_master_of_shared);
-    PDM_MPI_Comm_size(comm_master_of_shared, &n_rank_master_of_shared);
+  int i_rank_master_of_shm = -1;
+  int n_rank_master_of_shm;
+  if(comm_master_of_shm != PDM_MPI_COMM_NULL) {
+    PDM_MPI_Comm_rank(comm_master_of_shm, &i_rank_master_of_shm);
+    PDM_MPI_Comm_size(comm_master_of_shm, &n_rank_master_of_shm);
   }
-  PDM_MPI_Bcast(&n_rank_master_of_shared, 1, PDM_MPI_INT, 0, comm_shared);
-  PDM_MPI_Bcast(&i_rank_master_of_shared, 1, PDM_MPI_INT, 0, comm_shared);
+  PDM_MPI_Bcast(&n_rank_master_of_shm, 1, PDM_MPI_INT, 0, comm_shared);
+  PDM_MPI_Bcast(&i_rank_master_of_shm, 1, PDM_MPI_INT, 0, comm_shared);
 
-  PDM_mpi_win_shared_t* wnuma_by_numa_n = PDM_mpi_win_shared_create(n_rank_master_of_shared, sizeof(int), comm_shared);
+  PDM_mpi_win_shared_t* wnuma_by_numa_n = PDM_mpi_win_shared_create(n_rank_master_of_shm, sizeof(int), comm_shared);
   int *numa_by_numa_n  = PDM_mpi_win_shared_get(wnuma_by_numa_n);
   PDM_mpi_win_shared_lock_all (0, wnuma_by_numa_n);
 
 
-  if(comm_master_of_shared != PDM_MPI_COMM_NULL) {
-    PDM_MPI_Allgather(&n_rank_in_node  , 1, PDM_MPI_INT,
-                      numa_by_numa_n, 1, PDM_MPI_INT, comm_master_of_shared);
+  if(comm_master_of_shm != PDM_MPI_COMM_NULL) {
+    PDM_MPI_Allgather(&n_rank_in_shm, 1, PDM_MPI_INT,
+                      numa_by_numa_n, 1, PDM_MPI_INT, comm_master_of_shm);
   }
   PDM_mpi_win_shared_sync(wnuma_by_numa_n);
   PDM_MPI_Barrier(comm_shared);
 
   int n_tot_numa = 0;
-  for(int i = 0; i < n_rank_master_of_shared; ++i) {
+  for(int i = 0; i < n_rank_master_of_shm; ++i) {
     n_tot_numa += numa_by_numa_n[i];
   }
 
@@ -216,23 +216,23 @@ PDM_MPI_Comm setup_numa_graph2(PDM_MPI_Comm comm)
    * Create idx  and  gid of each numa
    */
   PDM_mpi_win_shared_t* wnuma_core_gid    = PDM_mpi_win_shared_create(n_tot_numa               , sizeof(int), comm_shared);
-  PDM_mpi_win_shared_t* wnuma_by_numa_idx = PDM_mpi_win_shared_create(n_rank_master_of_shared+1, sizeof(int), comm_shared);
+  PDM_mpi_win_shared_t* wnuma_by_numa_idx = PDM_mpi_win_shared_create(n_rank_master_of_shm+1, sizeof(int), comm_shared);
   int *numa_core_gid    = PDM_mpi_win_shared_get(wnuma_core_gid);
   int *numa_by_numa_idx = PDM_mpi_win_shared_get(wnuma_by_numa_idx);
   PDM_mpi_win_shared_lock_all (0, wnuma_core_gid);
   PDM_mpi_win_shared_lock_all (0, wnuma_by_numa_idx);
 
 
-  if(comm_master_of_shared != PDM_MPI_COMM_NULL) {
+  if(comm_master_of_shm != PDM_MPI_COMM_NULL) {
     numa_by_numa_idx[0] = 0;
-    for(int i = 0; i < n_rank_master_of_shared; ++i) {
+    for(int i = 0; i < n_rank_master_of_shm; ++i) {
       numa_by_numa_idx[i+1] = numa_by_numa_idx[i] + numa_by_numa_n[i];
     }
   }
   PDM_MPI_Barrier(comm_shared);
   PDM_mpi_win_shared_sync(wnuma_by_numa_idx);
 
-  numa_core_gid[numa_by_numa_idx[i_rank_master_of_shared]+i_rank_in_node] = i_rank;
+  numa_core_gid[numa_by_numa_idx[i_rank_master_of_shm]+i_rank_in_shm] = i_rank;
 
   PDM_MPI_Barrier(comm_shared);
   PDM_mpi_win_shared_sync(wnuma_core_gid);
@@ -240,19 +240,48 @@ PDM_MPI_Comm setup_numa_graph2(PDM_MPI_Comm comm)
   /*
    *  Exchange of the global numbering of rank for each NUMA
    */
-  if(comm_master_of_shared != PDM_MPI_COMM_NULL) {
-    int *lnuma_core_gid = malloc(n_rank_in_node * sizeof(int));
-    for(int i = 0; i < n_rank_in_node; ++i) {
-      lnuma_core_gid[i] = numa_core_gid[numa_by_numa_idx[i_rank_master_of_shared]+i];
+  if(comm_master_of_shm != PDM_MPI_COMM_NULL) {
+    int *lnuma_core_gid = malloc(n_rank_in_shm * sizeof(int));
+    for(int i = 0; i < n_rank_in_shm; ++i) {
+      lnuma_core_gid[i] = numa_core_gid[numa_by_numa_idx[i_rank_master_of_shm]+i];
     }
-    PDM_MPI_Allgatherv(lnuma_core_gid, n_rank_in_node, PDM_MPI_INT,
-                       numa_core_gid , numa_by_numa_n, numa_by_numa_idx, PDM_MPI_INT, comm_master_of_shared);
+    PDM_MPI_Allgatherv(lnuma_core_gid, n_rank_in_shm, PDM_MPI_INT,
+                       numa_core_gid , numa_by_numa_n, numa_by_numa_idx, PDM_MPI_INT, comm_master_of_shm);
     free(lnuma_core_gid);
   }
   PDM_MPI_Barrier(comm_shared);
   PDM_mpi_win_shared_sync(wnuma_core_gid);
 
-  PDM_log_trace_connectivity_int(numa_by_numa_idx, numa_core_gid, n_rank_master_of_shared, "numa_core_gid :: ");
+  PDM_log_trace_connectivity_int(numa_by_numa_idx, numa_core_gid, n_rank_master_of_shm, "numa_core_gid :: ");
+
+  /*
+   * Computation of in_degree
+   */
+  int n_degrees_in = 0;
+  for(int i = 0; i < n_rank_master_of_shm; ++i) {
+    for(int j = numa_by_numa_idx[i]; j < numa_by_numa_idx[i+1]; ++j) {
+      int lid_rank = (j - numa_by_numa_idx[i]) % n_rank_in_shm; // Donc numero de numa dans le group
+      if(lid_rank == i_rank_in_shm){
+        n_degrees_in++;
+      }
+    }
+  }
+
+  int* neighbor_in = malloc( (n_degrees_in ) * sizeof(int));
+  n_degrees_in = 0;
+  for(int i = 0; i < n_rank_master_of_shm; ++i) {
+    for(int j = numa_by_numa_idx[i]; j < numa_by_numa_idx[i+1]; ++j) {
+      int gid_rank = numa_core_gid[j];
+      int lid_rank = (j - numa_by_numa_idx[i]) % n_rank_in_shm;  // Donc numero de numa dans le group
+      if(lid_rank == i_rank_in_shm){
+        neighbor_in[n_degrees_in++] = gid_rank;
+      }
+    }
+  }
+
+  PDM_log_trace_array_int(neighbor_in, n_degrees_in, "neighbor_in ::");
+
+
 
 
   PDM_mpi_win_shared_unlock_all(wnuma_by_numa_n);
@@ -290,9 +319,9 @@ int main(int argc, char *argv[])
   PDM_MPI_Comm comm_shared;
   PDM_MPI_Comm_split_type(comm, PDM_MPI_SPLIT_NUMA, &comm_shared);
 
-  int n_rank_in_node, i_rank_in_node;
-  PDM_MPI_Comm_rank (comm_shared, &i_rank_in_node);
-  PDM_MPI_Comm_size (comm_shared, &n_rank_in_node);
+  int n_rank_in_shm, i_rank_in_shm;
+  PDM_MPI_Comm_rank (comm_shared, &i_rank_in_shm);
+  PDM_MPI_Comm_size (comm_shared, &n_rank_in_shm);
 
   /*
    * Setup graph
