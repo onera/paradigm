0857414f993651577bc73a73cb21e7c66d479c7f
Bastien Andrieu <bastien.andrieu@onera.fr>
2022-10-21 08:24:32 +0200

    clean up 2

--- test/pdm_t_dist_cloud_surf_from_stl.c
+++ test/pdm_t_dist_cloud_surf_from_stl.c
@@ -222,7 +222,7 @@ int main(int argc, char *argv[])
   PDM_g_num_t           n_g_pts_clouds = 10;
   double                length         = 1.;
   int                   n_part         = 1;
-  int                   post           = 1;
+  int                   post           = 0;
   char                 *filename  = NULL;
 
 #ifdef PDM_HAVE_PARMETIS
@@ -246,17 +246,11 @@ int main(int argc, char *argv[])
              &post,
      (int *) &part_method);
 
-  // if(filename == NULL) {
-  //   PDM_MPI_Finalize ();
-  //   return 0;
-  // }
   if (filename == NULL) {
     filename = (char *) "meshes/sphere.stl";
-    // printf("No file specified -> exit \n");
-    // return 0;
   }
 
-  double radius         = length;//2*length;
+  double radius = length;//2*length;
 
   /*
    *  Generate cloud
@@ -287,40 +281,10 @@ int main(int argc, char *argv[])
     fflush(stdout);
   }
 
-  // int         *dsurf_face_vtx_idx = NULL;
-  // PDM_g_num_t *dsurf_face_vtx     = NULL;
-  // double      *dsurf_vtx_coord    = NULL;
-  // PDM_g_num_t *surf_distrib_vtx   = NULL;
-  // PDM_g_num_t *surf_distrib_face  = NULL;
-
-  // _read_surface_mesh(comm,
-  //                    filename,
-  //                    &dsurf_face_vtx_idx,
-  //                    &dsurf_face_vtx,
-  //                    &dsurf_vtx_coord,
-  //                    &surf_distrib_vtx,
-  //                    &surf_distrib_face);
-
-  // // free(dsurf_face_vtx_idx); // Unused
-  // PDM_g_num_t  nsurf_g_vtx  = surf_distrib_vtx [n_rank];
-  // // PDM_g_num_t  nsurf_g_face = surf_distrib_face[n_rank];
-
-  // if (i_rank == 0) {
-  //   printf("-- Done \n");
-  //   fflush(stdout);
-  // }
-
-  // log_trace("nsurf_g_vtx = %i \n", nsurf_g_vtx);
-
-  // PDM_dmesh_nodal_t* dmn = _create_dmesh_nodal(comm,
-  //                                              surf_distrib_vtx,
-  //                                              surf_distrib_face,
-  //                                              dsurf_vtx_coord,
-  //                                              dsurf_face_vtx);
   PDM_dmesh_nodal_t* dmn = PDM_reader_stl_dmesh_nodal(comm,
                                                       filename);
 
-  PDM_multipart_t       *mpart_surf = NULL;
+  PDM_multipart_t *mpart_surf = NULL;
   _split_surface_mesh(comm,
                       part_method,
                       n_part,
@@ -479,18 +443,11 @@ int main(int argc, char *argv[])
   free(surf_pface_ln_to_gn);
 
 
-
-
-  // free(dsurf_face_vtx);
-  // free(dsurf_vtx_coord);
-  // free(surf_distrib_vtx);
-  // free(surf_distrib_face);
-
   PDM_DMesh_nodal_free(dmn);
 
   PDM_multipart_free(mpart_surf);
 
-  PDM_MPI_Barrier (PDM_MPI_COMM_WORLD);
+  PDM_MPI_Barrier (comm);
   if (i_rank == 0) {
   PDM_printf ("-- End\n");
   }
