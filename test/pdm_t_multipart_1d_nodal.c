1a167b2835b8649233031a7b1600ba712fafed1f
Julien Coulet <julien.coulet@onera.fr>
2023-04-28 18:48:37 +0200

    Add _compute_part_mesh_nodal_1d in multipart

--- test/pdm_t_multipart_1d_nodal.c
+++ test/pdm_t_multipart_1d_nodal.c
@@ -269,6 +269,9 @@ char *argv[]
   PDM_multipart_register_dmesh_nodal(mpart, 0, dmn);
 
   PDM_multipart_run_ppart(mpart);
+  PDM_part_mesh_nodal_t *pmn = NULL;
+  PDM_multipart_get_part_mesh_nodal(mpart, 0, &pmn, PDM_OWNERSHIP_KEEP);
+  PDM_part_mesh_nodal_free(pmn);
 
   if(0 == 1) {
     for(int i_part = 0; i_part < n_part; ++i_part) {
