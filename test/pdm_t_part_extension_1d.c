87749d4a5fdc643985b30f4215c5ceb4d56df32b
Bruno Maugars <bruno.maugars@onera.fr>
2023-07-24 10:19:17 +0200

    [pdm_part_extension] Manage creation of part_to_part for join between partition

--- test/pdm_t_part_extension_1d.c
+++ test/pdm_t_part_extension_1d.c
@@ -23,6 +23,8 @@
 #include "pdm_distrib.h"
 #include "pdm_vtk.h"
 #include "pdm_multipart.h"
+#include "pdm_array.h"
+#include "pdm_part_to_part.h"
 
 /*============================================================================
  * Macro definitions
@@ -130,6 +132,80 @@ _read_args
 }
 
 static
+PDM_g_num_t*
+_compute_offset_ln_to_gn_by_domain
+(
+  int              n_domain,
+  int             *n_part,
+  int            **pn_entity,
+  PDM_g_num_t   ***pentity_ln_to_gn,
+  PDM_MPI_Comm     comm
+)
+{
+
+  PDM_g_num_t *shift_by_domain_loc = PDM_array_const_gnum(n_domain, 0);
+  PDM_g_num_t *shift_by_domain     = (PDM_g_num_t *) malloc((n_domain+1) * sizeof(PDM_g_num_t));
+
+  for(int i_domain = 0; i_domain < n_domain; ++i_domain) {
+    for(int i_part = 0; i_part < n_part[i_domain]; ++i_part) {
+
+      int          _pn_entity        = pn_entity       [i_domain][i_part];
+      PDM_g_num_t *_pentity_ln_to_gn = pentity_ln_to_gn[i_domain][i_part];
+      for(int i = 0; i < _pn_entity; ++i) {
+        shift_by_domain_loc[i_domain] = PDM_MAX(shift_by_domain_loc[i_domain], _pentity_ln_to_gn[i]);
+      }
+    }
+  }
+
+  shift_by_domain[0] = 0;
+  PDM_MPI_Allreduce(shift_by_domain_loc, &shift_by_domain[1], n_domain, PDM__PDM_MPI_G_NUM, PDM_MPI_MAX, comm);
+  PDM_array_accumulate_gnum(shift_by_domain, n_domain+1);
+
+  free(shift_by_domain_loc);
+
+  return shift_by_domain;
+}
+
+// static
+// void
+// _shift_ln_to_gn
+// (
+//   int          n_entity,
+//   PDM_g_num_t *entity_ln_to_gn,
+//   PDM_g_num_t  shift,
+//   int          sens
+// )
+// {
+//   for(int i = 0; i < n_entity; ++i) {
+//     entity_ln_to_gn[i] = entity_ln_to_gn[i] + sens * shift;
+//   }
+// }
+
+static
+void
+_offset_ln_to_gn_by_domain
+(
+  int              n_domain,
+  int             *n_part,
+  int            **pn_entity,
+  PDM_g_num_t   ***pentity_ln_to_gn,
+  PDM_g_num_t     *shift_by_domain,
+  int              sens
+)
+{
+  for(int i_domain = 0; i_domain < n_domain; ++i_domain) {
+    for(int i_part = 0; i_part < n_part[i_domain]; ++i_part) {
+      int          _pn_entity        = pn_entity       [i_domain][i_part];
+      PDM_g_num_t *_pentity_ln_to_gn = pentity_ln_to_gn[i_domain][i_part];
+      for(int i = 0; i < _pn_entity; ++i) {
+        _pentity_ln_to_gn[i] = _pentity_ln_to_gn[i] + sens * shift_by_domain[i_domain];
+      }
+    }
+  }
+}
+
+
+static
 void
 _part_extension
 (
@@ -143,6 +219,7 @@ _part_extension
 {
   PDM_UNUSED(n_depth);
   PDM_UNUSED(mpart);
+  PDM_UNUSED(pdi);
 
   int i_rank;
   int n_rank;
@@ -152,8 +229,75 @@ _part_extension
   int* n_part_g = malloc(n_domain * sizeof(int));
   PDM_MPI_Allreduce(n_part, n_part_g, n_domain, PDM_MPI_INT, PDM_MPI_SUM, comm);
 
+  int          **pn_vtx             = (int          **) malloc( n_domain * sizeof(int          *));
+  PDM_g_num_t ***pvtx_ln_to_gn      = (PDM_g_num_t ***) malloc( n_domain * sizeof(PDM_g_num_t **));
+  int          *pflat_n_vtx         = (int           *) malloc( n_domain * sizeof(int           ));
+  PDM_g_num_t **pflat_vtx_ln_to_gn  = (PDM_g_num_t  **) malloc( n_domain * sizeof(PDM_g_num_t  *));
+
+  int ln_part_tot = 0;
+  for(int i_dom = 0; i_dom < n_domain; ++i_dom) {
+
+    pn_vtx       [i_dom]  = (int          *) malloc( n_part[i_dom] * sizeof(int          ));
+    pvtx_ln_to_gn[i_dom]  = (PDM_g_num_t **) malloc( n_part[i_dom] * sizeof(PDM_g_num_t *));
+
+    for(int i_part = 0; i_part < n_part[i_dom]; ++i_part) {
+
+      pn_vtx[i_dom][i_part] = PDM_multipart_part_ln_to_gn_get(mpart,
+                                                              i_dom,
+                                                              i_part,
+                                                              PDM_MESH_ENTITY_VERTEX,
+                                                              &pvtx_ln_to_gn[i_dom][i_part],
+                                                              PDM_OWNERSHIP_KEEP);
+
+      pflat_n_vtx       [ln_part_tot+i_part] = pn_vtx       [i_dom][i_part];
+      pflat_vtx_ln_to_gn[ln_part_tot+i_part] = pvtx_ln_to_gn[i_dom][i_part];
 
 
+
+    }
+    ln_part_tot += n_part[i_dom];
+  }
+
+  PDM_g_num_t* shift_by_domain_vtx = _compute_offset_ln_to_gn_by_domain(n_domain,
+                                                                        n_part,
+                                                                        pn_vtx,
+                                                                        pvtx_ln_to_gn,
+                                                                        comm);
+
+  /* Shift ln_to_gn */
+  _offset_ln_to_gn_by_domain(n_domain,
+                             n_part,
+                             pn_vtx,
+                             pvtx_ln_to_gn,
+                             shift_by_domain_vtx,
+                             1);
+
+  for(int i_dom = 0; i_dom < n_domain; ++i_dom) {
+    for(int i_part = 0; i_part < n_part[i_dom]; ++i_part) {
+      int *ppart_vtx_proc_idx = NULL;
+      int *ppart_vtx_part_idx = NULL;
+      int *ppart_vtx          = NULL; // (i_vtx, i_proc, i_part, i_vtx_opp)
+      PDM_multipart_part_graph_comm_get(mpart,
+                                        i_dom,
+                                        i_part,
+                                        PDM_BOUND_TYPE_VTX,
+                                        &ppart_vtx_proc_idx,
+                                        &ppart_vtx_part_idx,
+                                        &ppart_vtx,
+                                        PDM_OWNERSHIP_KEEP);
+
+      // PDM_log_trace_array_int(ppart_vtx, 4 * ppart_vtx_part_idx[n_part_g[i_dom]], "ppart_vtx");
+
+    }
+  }
+
+  //
+  int         **part1_to_part2_idx         = malloc(ln_part_tot * sizeof(int *));
+  int         **part1_to_part2_triplet_idx = NULL; //malloc(ln_part_tot * sizeof(int *));
+  int         **part1_to_part2_triplet     = malloc(ln_part_tot * sizeof(int *));
+
+  // Count
+  int li_part = 0;
   for(int i_dom = 0; i_dom < n_domain; ++i_dom) {
     for(int i_part = 0; i_part < n_part[i_dom]; ++i_part) {
       int *ppart_vtx_proc_idx = NULL;
@@ -170,11 +314,113 @@ _part_extension
 
       PDM_log_trace_array_int(ppart_vtx, 4 * ppart_vtx_part_idx[n_part_g[i_dom]], "ppart_vtx");
 
+      part1_to_part2_idx[li_part] = malloc((pflat_n_vtx[li_part] + 1) *sizeof(int));
+      part1_to_part2_idx[li_part][0] = 0;
+
+      int *part1_to_part2_n = PDM_array_zeros_int(pflat_n_vtx[li_part]);
+
+      int n_entity_bound = ppart_vtx_part_idx[n_part_g[i_dom]];
+
+      for(int idx_entity = 0; idx_entity < n_entity_bound; ++idx_entity) {
+        int i_entity = ppart_vtx[4*idx_entity]-1;
+        part1_to_part2_n[i_entity] += 1;
+      }
+
+      PDM_log_trace_array_int(part1_to_part2_n, pflat_n_vtx[li_part], "part1_to_part2_n ::");
+
+      for(int i_entity = 0; i_entity < pflat_n_vtx[li_part]; ++i_entity) {
+        part1_to_part2_idx[li_part][i_entity+1] = part1_to_part2_idx[li_part][i_entity] + 3 * part1_to_part2_n[i_entity];
+        part1_to_part2_n[i_entity] = 0;
+      }
+
+      int n_connect_tot = part1_to_part2_idx[li_part][pflat_n_vtx[li_part]];
+      part1_to_part2_triplet[li_part] = malloc(n_connect_tot * sizeof(int));
+
+      for(int idx_entity = 0; idx_entity < n_entity_bound; ++idx_entity) {
+        int i_entity     = ppart_vtx[4*idx_entity]-1;
+        int i_proc_opp   = ppart_vtx[4*idx_entity+1];
+        int i_part_opp   = ppart_vtx[4*idx_entity+2]-1; // A gere -> Le shift
+        int i_entity_opp = ppart_vtx[4*idx_entity+3]-1;
+
+        int idx_write = part1_to_part2_idx[li_part][i_entity] + part1_to_part2_n[i_entity]++;
+        part1_to_part2_triplet[li_part][idx_write  ] = i_proc_opp;
+        part1_to_part2_triplet[li_part][idx_write+1] = i_part_opp;
+        part1_to_part2_triplet[li_part][idx_write+2] = i_entity_opp;
+
+        // part1_to_part2_triplet[li_part][idx_entity+1] = part1_to_part2_triplet[li_part][idx_entity] + 3;
+      }
+
+      PDM_log_trace_array_int(part1_to_part2_idx    [li_part],     pflat_n_vtx[li_part], "part1_to_part2_idx ::");
+      PDM_log_trace_array_int(part1_to_part2_triplet[li_part],     n_connect_tot       , "part1_to_part2_triplet ::");
+
+      free(part1_to_part2_n);
+      li_part += 1;
     }
   }
 
+
+  // Attention à l'histoire du x3 dans l'idx
+  PDM_part_to_part_t* ptp = PDM_part_to_part_create_from_num2_triplet((const PDM_g_num_t **) pflat_vtx_ln_to_gn,
+                                                                      (const int          *) pflat_n_vtx,
+                                                                      ln_part_tot,
+                                                                      (const int          *) pflat_n_vtx,
+                                                                      ln_part_tot,
+                                                                      (const int         **) part1_to_part2_idx,
+                                                                      (const int         **) part1_to_part2_triplet_idx,
+                                                                      (const int         **) part1_to_part2_triplet,
+                                                                      comm);
+
+
+  int  *n_ref_lnum2 = NULL;
+  int **ref_lnum2   = NULL;
+  PDM_part_to_part_ref_lnum2_get(ptp, &n_ref_lnum2, &ref_lnum2);
+
+  if(1 == 1) {
+    for(int i_part = 0; i_part < ln_part_tot; ++i_part) {
+      PDM_log_trace_array_int(ref_lnum2[i_part], n_ref_lnum2[i_part], "ref_lnum2 :");
+    }
+  }
+
+  PDM_part_to_part_free(ptp);
+
+  // Objectif :
+  //   - Faire un part_to_part qui englobe les raccords entre domain et entre partition
+  //   - ATTENTION au piege du shift
+
+  // A faire :
+  //  - Shifter tout les ln_to_gn
+
+  // On échange le numero de domain pour retrouver le lien partition -> domaine opposé !
+  // On échange le kind aussi pour dissocié périodicité/interface entre domaine/interface entre partition
+
+  /* Unshift ln_to_gn */
+  _offset_ln_to_gn_by_domain(n_domain,
+                             n_part,
+                             pn_vtx,
+                             pvtx_ln_to_gn,
+                             shift_by_domain_vtx,
+                             -1);
+
+  for(int i_part = 0; i_part < ln_part_tot; ++i_part) {
+    free(part1_to_part2_idx        [i_part]);
+    // free(part1_to_part2_triplet_idx[i_part]);
+    // free(part1_to_part2_triplet    [i_part]);
+  }
+  free(part1_to_part2_idx        );
+  // free(part1_to_part2_triplet_idx);
+  free(part1_to_part2_triplet    );
+
+  free(shift_by_domain_vtx);
   free(n_part_g);
 
+  for(int i_dom = 0; i_dom < n_domain; ++i_dom) {
+    free(pn_vtx       [i_dom]);
+    free(pvtx_ln_to_gn[i_dom]);
+  }
+  free(pn_vtx);
+  free(pvtx_ln_to_gn);
+  free(pflat_n_vtx       );
+  free(pflat_vtx_ln_to_gn);
 }
 
 
