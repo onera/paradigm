cbbd586b20c4e83e222d1f839670d0ff9cd89e2d
Karmijn Hoogveld <karmijn.hoogveld@onera.fr>
2023-10-09 16:57:43 +0200

    [tutorial] start partitioning in Fortran

--- test/tutorial/partitioning/pdm_t_mesh_partitioning_sol_f.F90
+++ test/tutorial/partitioning/pdm_t_mesh_partitioning_sol_f.F90
@@ -27,7 +27,7 @@ program pdm_t_mesh_partitioning_sol_f
 #endif
   use pdm_multipart
   use pdm_vtk
-  ! use pdm_dcube_nodal_gen
+  use pdm_dcube_nodal_gen
   use pdm_mesh_nodal
   use iso_c_binding
   use pdm_fortran
@@ -39,10 +39,40 @@ program pdm_t_mesh_partitioning_sol_f
 #endif
 
   !-----------------------------------------------------------
+  integer (c_int)                       :: i
+
+  ! MPI
   integer                               :: code
   integer                               :: i_rank
   integer                               :: n_rank
   integer, parameter                    :: comm = MPI_COMM_WORLD
+
+  ! PDM_dcube_nodal_gen_create
+  integer                               :: n_x, n_y, n_z
+  integer                               :: elt_type, order, ownership
+  double precision                      :: length
+  double precision                      :: xmin, ymin, zmin
+  type (c_ptr)                          :: dcube
+
+  ! PDM_dcube_nodal_gen_dmesh_nodal_get
+  type (c_ptr)                          :: dmn
+
+  ! PDM_multipart_create
+  type (c_ptr)                          :: mpart
+  integer (c_int)                       :: n_zone = 1
+  integer(kind=PDM_l_num_s), pointer    :: n_part(:) => null()
+  integer (c_int)                       :: merge_blocks
+  integer (c_int)                       :: split_method
+  integer (c_int)                       :: part_size_method
+  double precision,          pointer    :: part_fraction(:) => null()
+
+  integer (c_int)                       :: i_zone    = 0
+  integer (c_int)                       :: i_section = 0
+  integer (c_int)                       :: i_part    = 0
+
+  ! PDM_multipart_set_reordering_options
+  integer(kind=PDM_l_num_s), pointer    :: renum_cell_properties(:) => null()
+
   !-----------------------------------------------------------
 
   ! Initialize MPI environment
@@ -50,7 +80,74 @@ program pdm_t_mesh_partitioning_sol_f
   call mpi_comm_rank(comm, i_rank, code)
   call mpi_comm_size(comm, n_rank, code)
 
-  ! TO DO
+  ! Generate block-distributed parallelepided mesh
+  n_x      = 10
+  n_y      = 10
+  n_z      = 10
+  length   = 1.
+  xmin     = 0.
+  ymin     = 0.
+  zmin     = 0.
+  elt_type = 4 ! TO DO : how to use PDM_MESH_NODAL_TETRA4 in Fortran ?
+  order    = 1
+  call PDM_dcube_nodal_gen_create(dcube,     &
+                                  comm,      &
+                                  n_x,       &
+                                  n_y,       &
+                                  n_z,       &
+                                  length,    &
+                                  xmin,      &
+                                  ymin,      &
+                                  zmin,      &
+                                  elt_type,  &
+                                  order,     &
+                                  ownership) ! TO DO PDM_OWNERSHIP_USER
+
+  call PDM_dcube_nodal_gen_build(dcube, dmn)
+
+  call PDM_dcube_nodal_gen_dmesh_nodal_get(dcube, dmn)
+
+  ! call PDM_dmesh_nodal_generate_distribution(dmn)
+  ! TO DO : interface Fortran de dmesh_nodal
+
+  call PDM_dcube_nodal_gen_free(dcube)
+
+  ! Create partitioning object
+  allocate(n_part(n_zone))
+
+  do i = 1, n_zone
+    n_part(i) = 1
+  end do
+
+  call PDM_multipart_create(mpart,            &
+                            n_zone,           &
+                            n_part,           &
+                            merge_blocks,     &
+                            split_method,     &
+                            part_size_method, &
+                            part_fraction,    &
+                            comm,             &
+                            ownership) ! TO DO PDM_OWNERSHIP_KEEP
+
+  call PDM_multipart_set_reordering_options(mpart,                      &
+                                            i_zone,                     &
+                                            "PDM_PART_RENUM_CELL_NONE", &
+                                            renum_cell_properties,      &
+                                            "PDM_PART_RENUM_FACE_NONE")
+
+  call PDM_multipart_register_dmesh_nodal(mpart,  &
+                                          i_zone, &
+                                          dmn)
+
+  call PDM_multipart_run_ppart(mpart)
+
+  ! Get mesh arrrays in FE structure
+
+  ! Get mesh arrrays in FV structure
+
+  ! free
+  ! call PDM_DMesh_nodal_free(dmn)
+  call PDM_multipart_free(mpart)
 
   ! Finalize MPI environment
   call mpi_finalize(code)
