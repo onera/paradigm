5d99e3bf34e5131489255c4ec1d5595da11e6bee
Karmijn Hoogveld <karmijn.hoogveld@onera.fr>
2023-11-09 17:20:00 +0100

    [pasc24] print ranks per node

--- test/pdm_t_block_to_part_p.py
+++ test/pdm_t_block_to_part_p.py
@@ -14,16 +14,16 @@ i_rank = comm.rank
 n_rank = comm.size
 
 # Size
-size = 100 # 200 000
+size = 100
 size_per_rank = size // n_rank
 
 # Partition distribution
 diagonal = 0
-circulante = 0
-aleatoire = 1
+circulante = 1
+aleatoire = 0
+compact_sort = 0
 
 # Block
-# use : compute_weighted_distribution
 block_distribution = np.array([i*size_per_rank for i in range(n_rank+1)]).astype(PDM.npy_pdm_gnum_dtype)
 block_data = np.array([1 for i in range(size_per_rank)]).astype(np.intc)
 
@@ -39,6 +39,17 @@ if circulante:
 if aleatoire: # int not long int
   part_ln_to_gn = np.array([random.randint(1, n_rank*size_per_rank) for i in range(size_per_rank)]).astype(PDM.npy_pdm_gnum_dtype)
 
+if compact_sort:
+  # to do
+  # rank 0 recoit les valeurs de tous les ranks
+  # fait le trie
+  # envoie aux ranks leur gnum
+  alpha = 0.4
+  part_value = i_rank + alpha * (np.random.rand(size_per_rank)* 2 - 1)
+  part_ln_to_gn = np.argsort(part_value)
+  part_ln_to_gn = i_rank*size_per_rank + part_ln_to_gn.astype(PDM.npy_pdm_gnum_dtype) + 1
+  print(part_ln_to_gn)
+
 # Create BTP
 btp = PDM.BlockToPart(block_distribution,
                       comm,
@@ -47,3 +58,13 @@ btp = PDM.BlockToPart(block_distribution,
 
 # Exchange
 part_stride, part_data = btp.exchange_field(block_data)
+
+# Rank per node
+shared_comm = comm.Split_type(MPI.COMM_TYPE_SHARED)
+
+i_shared_rank = shared_comm.rank
+
+data = shared_comm.gather(i_rank,root=0)
+
+if i_shared_rank == 0:
+   print("group {} has ranks {}\n".format(i_rank, data))
