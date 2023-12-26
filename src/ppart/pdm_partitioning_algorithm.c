54aa740845e5d22cc4dd5cfe1b18fb44ff74c0f0
Bruno Maugars <bruno.maugars@onera.fr>
2023-12-26 13:20:23 +0100

    [pdm_multipart/pdm_extract_part] Enable renumbering for extract_part - Add API to renumbering all mesh entity in pdm_multipart

--- src/ppart/pdm_partitioning_algorithm.c
+++ src/ppart/pdm_partitioning_algorithm.c
@@ -296,6 +296,7 @@ PDM_part_assemble_partitions
       (*pentity_ln_to_gn)[i_part] = NULL;
     }
     if(pentity_init_location != NULL) {
+      free(*pentity_init_location);
       *pentity_init_location = (int **) malloc( sizeof(int *) * dn_part);
       for(int i_part = 0; i_part < dn_part; ++i_part) {
         (*pentity_init_location)[i_part] = NULL;
