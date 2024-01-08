68e04e24b63a0aa55ee03ed61f23dbb3c8bd3ab7
Karmijn Hoogveld <karmijn.hoogveld@onera.fr>
2024-01-08 10:19:43 +0100

    [hpdc24] corr

--- test/pdm_t_block_to_part_p.py
+++ test/pdm_t_block_to_part_p.py
@@ -45,9 +45,9 @@ randomize = args.randomize
 if randomize:
   part_ln_to_gn = np.random.randint(1, size * n_rank + 1, size, dtype=PDM.npy_pdm_gnum_dtype)
 else:
-  beg = ((i_rank)%n_rank)*size - shift_size + 1
+  beg = ((i_rank)%n_rank)*size - shift_size*size + 1
   beg = max(1, min(beg, n_rank*size))
-  end = ((i_rank)%n_rank)*size + shift_size + size
+  end = ((i_rank)%n_rank)*size + shift_size*size + size
   end = max(1, min(end, n_rank*size))
   part_ln_to_gn = np.random.randint(beg, end+1, size, dtype=PDM.npy_pdm_gnum_dtype)
 
@@ -80,3 +80,5 @@ for i in range(n_iter):
 PDM.btp_time_per_step_dump(comm,
                            filename)
 
+if i_rank == 0:
+  print("End :)")
