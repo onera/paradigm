38580ab317b6c1c3a670f879c927ddfa80f34877
Karmijn Hoogveld <karmijn.hoogveld@onera.fr>
2024-04-30 18:03:16 +0200

    [warning] rm in Triangle

--- src/mesh/pdm_triangle.c
+++ src/mesh/pdm_triangle.c
@@ -1112,9 +1112,9 @@ PDM_triangle_inscribed_circle
  const double*  vtx_coord[3]
 )
 {
-  const double *a = vtx_coord[0];
-  const double *b = vtx_coord[1];
-  const double *c = vtx_coord[2];
+  double *a = (double *) vtx_coord[0];
+  double *b = (double *) vtx_coord[1];
+  double *c = (double *) vtx_coord[2];
 
   double ab[3] = {b[0] - a[0],
                   b[1] - a[1],
