da74bbf26b95da30d2ecbec8c57b470245687b44
Clément Benazet <clement.benazet@onera.fr>
2025-04-08 13:29:27 +0200

    [general] fix compilation warnings

--- src/meshgen/test/dcube_nodal_gen.test.cpp
+++ src/meshgen/test/dcube_nodal_gen.test.cpp
@@ -434,8 +434,6 @@ MPI_TEST_CASE("[dcube_nodal_gen] - 1p - quad ",1) {
 
   PDM_dmesh_nodal_t* dmesh_nodal = PDM_dcube_nodal_gen_dmesh_nodal_get(dcube); /* It will be free by PDM_dcube_nodal_gen_free because PDM_OWNERSHIP_KEEP */
 
-  double *vtx_coords = PDM_DMesh_nodal_vtx_get(dmesh_nodal, PDM_OWNERSHIP_BAD_VALUE);
-
   /* Verif global */
   PDM_g_num_t n_cell_abs = -100;
   PDM_g_num_t n_face_abs = -100;
