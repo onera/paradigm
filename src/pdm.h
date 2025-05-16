ccdcf6ffc106a57dd888ca98876f754a0e257f6f
Bastien Andrieu <bastien.andrieu@onera.fr>
2025-05-16 17:51:15 +0200

    [multipart] remove privates in Fortran API

--- src/pdm.h
+++ src/pdm.h
@@ -323,7 +323,7 @@ typedef enum {
   PDM_SPLIT_DUAL_WITH_PARMETIS = 1, /*!< Use the <a href="https://github.com/KarypisLab/ParMETIS">ParMETIS</a> graph partitioning library */
   PDM_SPLIT_DUAL_WITH_PTSCOTCH = 2, /*!< Use the <a href="https://gitlab.inria.fr/scotch/scotch">PT-Scotch</a> graph partitioning library */
   PDM_SPLIT_DUAL_WITH_HILBERT  = 3, /*!< Use in-house method based on the <a href="https://en.wikipedia.org/wiki/Hilbert_curve">Hilbert space-filling</a> curve */
-  PDM_SPLIT_DUAL_WITH_IMPLICIT = 4  /*!< Split into contiguous chunks of global ids */
+  PDM_SPLIT_DUAL_WITH_IMPLICIT = 4  /*!< Split into contiguous chunks of global IDs */
 } PDM_split_dual_t;
 
 /**
