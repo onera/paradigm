3c6076346029cd921b99ac07c3d2fab5048f686b
Bastien Andrieu <bastien.andrieu@onera.fr>
2025-07-16 18:39:19 +0200

    [linear_programming] add unit test for PDM_lp_pts_inside_convex_hull

--- src/struct/test/pdm_linear_programming.test.cpp
+++ src/struct/test/pdm_linear_programming.test.cpp
@@ -60,3 +60,83 @@ TEST_CASE("[pdm_linear_programming] - PDM_lp_solve_nd 2d") {
 }
 
 
+
+
+
+TEST_CASE("[pdm_linear_programming] - PDM_lp_pts_inside_convex_hull 2d") {
+
+  const int dim   = 2;
+  const int n_src = 8;
+  const int n_tgt = 3;
+
+  double src_coord[n_src*3] = {
+     0.0,  0.0, 0,
+    12.0,  2.0, 0,
+     4.0,  4.0, 0,
+    14.0,  8.0, 0,
+    10.0,  6.0, 0,
+     4.0, 10.0, 0,
+     8.0,  2.0, 0,
+     0.0,  6.0, 0
+  };
+
+
+  double tgt_coord[n_tgt*3] = {
+     2.0,  2.0, 0, // inside convex hull
+    18.0,  2.0, 0, // outside AABB
+    10.0, 10.0, 0  // inside  AABB but outside convex hull
+  };
+
+  int tgt_status[n_tgt];
+  PDM_lp_pts_inside_convex_hull(dim,
+                                n_src,
+                                src_coord,
+                                n_tgt,
+                                tgt_coord,
+                                tgt_status);
+
+  int expected_tgt_status[n_tgt] = {1, 0, 0};
+  CHECK_EQ_C_ARRAY(tgt_status, expected_tgt_status, n_tgt);
+}
+
+
+
+TEST_CASE("[pdm_linear_programming] - PDM_lp_pts_inside_convex_hull 3d") {
+
+  const int dim   = 3;
+  const int n_src = 12;
+  const int n_tgt = 3;
+
+  double src_coord[n_src*3] = {
+     2.0, 4.0, -1.0,
+     1.0, 2.0,  2.0,
+    -1.0, 0.0,  1.0,
+     1.0, 1.0,  1.0,
+     0.0, 1.0,  0.0,
+    -1.0, 3.0,  0.0,
+    -1.0, 1.0,  2.0,
+     3.0, 1.0, -1.0,
+    -1.0, 1.0, -1.0,
+     2.0, 2.0,  0.0,
+     1.0, 2.0, -1.0,
+     1.0, 1.0,  0.0
+  };
+
+
+  double tgt_coord[n_tgt*3] = {
+    0.0, 1.0, 0.0, // inside convex hull
+    4.0, 1.0, 1.0, // outside AABB
+    1.0, 3.0, 1.0  // inside  AABB but outside convex hull
+  };
+
+  int tgt_status[n_tgt];
+  PDM_lp_pts_inside_convex_hull(dim,
+                                n_src,
+                                src_coord,
+                                n_tgt,
+                                tgt_coord,
+                                tgt_status);
+
+  int expected_tgt_status[n_tgt] = {1, 0, 0};
+  CHECK_EQ_C_ARRAY(tgt_status, expected_tgt_status, n_tgt);
+}
