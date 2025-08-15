ebc68942b6fe8a424591253651e87b1c89e2e653
Nicolas Dellinger <nicolas.dellinger@onera.fr>
2025-08-15 16:41:09 +0200

    minor fixes

--- cmake/modules/TestCreate.cmake
+++ cmake/modules/TestCreate.cmake
@@ -184,11 +184,11 @@ function(test_fortran_create name n_proc LIST_TEST LIST_NRANK)
 
   if(PDM_ENABLE_STATIC AND NOT PDM_ENABLE_SHARED)
     target_link_libraries(${name} PRIVATE pdmf_STATIC)
-    get_target_property(MODDIR pdmf_SHARED Fortran_MODULE_DIRECTORY)
+    get_target_property(MODDIR pdmf Fortran_MODULE_DIRECTORY)
   endif()
   if(PDM_ENABLE_SHARED)
     target_link_libraries(${name} PRIVATE pdmf_SHARED)
-    get_target_property(MODDIR pdmf_SHARED Fortran_MODULE_DIRECTORY)
+    get_target_property(MODDIR pdmf Fortran_MODULE_DIRECTORY)
   endif()
 
   target_include_directories(${name} PRIVATE ${MODDIR})
