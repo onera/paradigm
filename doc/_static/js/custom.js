a8dcd6d149b332e2bd619f32a3180172d007c486
Bastien Andrieu <bastien.andrieu@onera.fr>
2025-08-21 14:53:16 +0200

    [doc] cleaner custom.js

--- doc/_static/js/custom.js
+++ doc/_static/js/custom.js
@@ -4,23 +4,23 @@
  * We add extra br tags to the autodoc output, so each parameter is shown on
  * its own line.
  */
-function setupAutodocPy() {
+function setup_autodoc_py() {
   const paramElements = document.querySelectorAll('.py .sig-param')
 
   Array(...paramElements).forEach((element) => {
-      let brElement = document.createElement('br')
-      element.parentNode.insertBefore(brElement, element)
+    let brElement = document.createElement('br')
+    element.parentNode.insertBefore(brElement, element)
   })
 
   const lastParamElements = document.querySelectorAll('.py em.sig-param:last-of-type')
 
   Array(...lastParamElements).forEach((element) => {
-      let brElement = document.createElement('br')
-      element.after(brElement)
+    let brElement = document.createElement('br')
+    element.after(brElement)
   })
 }
 
-function setupAutodocCppF() {
+function setup_autodoc_cpp_f() {
   // const highlightableElements = document.querySelectorAll(".c dt.sig-object, .cpp dt.sig-object")
 
   // Array(...highlightableElements).forEach((element) => {
@@ -30,35 +30,34 @@ function setupAutodocCppF() {
   const documentables = document.querySelectorAll("dt.sig-object.c,dt.sig-object.cpp,dt.sig-object.f");
 
   Array(...documentables).forEach((element) => {
-      // element.classList.add("highlight");
+    // element.classList.add("highlight");
 
-      var parens = element.querySelectorAll(".sig-paren");
-      var commas = Array(...element.childNodes).filter(e => e.textContent == ", ")
+    var parens = element.querySelectorAll(".sig-paren");
+    var commas = Array(...element.childNodes).filter(e => e.textContent == ", ")
 
-      if (parens.length != 2) return;
+    if (parens.length != 2) return;
 
-      commas.forEach(c => {
-          if (c.compareDocumentPosition(parens[0]) == Node.DOCUMENT_POSITION_PRECEDING &&
-              c.compareDocumentPosition(parens[1]) == Node.DOCUMENT_POSITION_FOLLOWING
-          ) {
-              let brElement   = document.createElement('br')
-              let spanElement = document.createElement('span')
-              spanElement.className = "sig-indent"
-              c.after(brElement)
-              brElement.after(spanElement)
-          }
-      });
-
-      if (parens[0].nextSibling != parens[1]) {
-          // not an empty argument list
-          let brElement   = document.createElement('br')
-          let spanElement = document.createElement('span')
-          spanElement.className = "sig-indent"
-          parens[0].after(brElement)
-          brElement.after(spanElement)
-          let brElement1 = document.createElement('br')
-          parens[1].parentNode.insertBefore(brElement1, parens[1]);
+    commas.forEach(c => {
+      if (c.compareDocumentPosition(parens[0]) == Node.DOCUMENT_POSITION_PRECEDING &&
+          c.compareDocumentPosition(parens[1]) == Node.DOCUMENT_POSITION_FOLLOWING) {
+        let brElement   = document.createElement('br')
+        let spanElement = document.createElement('span')
+        spanElement.className = "sig-indent"
+        c.after(brElement)
+        brElement.after(spanElement)
       }
+    });
+
+    if (parens[0].nextSibling != parens[1]) {
+      // not an empty argument list
+      let brElement   = document.createElement('br')
+      let spanElement = document.createElement('span')
+      spanElement.className = "sig-indent"
+      parens[0].after(brElement)
+      brElement.after(spanElement)
+      let brElement1 = document.createElement('br')
+      parens[1].parentNode.insertBefore(brElement1, parens[1]);
+    }
   })
 }
 
@@ -66,7 +65,7 @@ function setupAutodocCppF() {
  * Ugly patch for Fortran subroutines
  * Make a specific class for better highlighting since sphin-fortran does not
  */
-function fixAutodocFortranSubroutines() {
+function fix_autodoc_fortran_subroutines() {
   const documentables = document.querySelectorAll("dl");
 
   Array(...documentables).forEach((element) => {
@@ -75,7 +74,7 @@ function fixAutodocFortranSubroutines() {
     if (children.length <= 2) return;
 
     if (children[1].className == "sig sig-object f") {
-        element.className = "fortran subroutine"
+      element.className = "fortran subroutine"
     }
 
   });
@@ -85,7 +84,7 @@ function fixAutodocFortranSubroutines() {
 document.addEventListener("DOMContentLoaded", function() {
   console.log("custom theme loaded")
 
-  setupAutodocPy()
-  setupAutodocCppF()
-  fixAutodocFortranSubroutines()
+  setup_autodoc_py()
+  setup_autodoc_cpp_f()
+  fix_autodoc_fortran_subroutines()
 })
