e803f3cf6e28595505aec24a08ecc0bc6cb4a987
Bastien Andrieu <bastien.andrieu@onera.fr>
2025-09-22 10:34:48 +0200

    [extract_part] test extraction of volume groups in nodal mode

--- test/pdm_t_extract_part_nodal_from_target.c
+++ test/pdm_t_extract_part_nodal_from_target.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "pdm.h"
+#include "pdm_array.h"
 #include "pdm_dcube_nodal_gen.h"
 #include "pdm_distrib.h"
 #include "pdm_dmesh.h"
@@ -402,6 +403,66 @@ _gen_mesh
 
   PDM_DMesh_nodal_free(dmn);
   PDM_multipart_free(mpart);
+
+
+  PDM_geometry_kind_t geom_kind = PDM_part_mesh_nodal_principal_geom_kind_get(pmn);
+
+  if (geom_kind == PDM_GEOMETRY_KIND_VOLUMIC) {
+    PDM_part_mesh_nodal_elmts_t *pmne = PDM_part_mesh_nodal_part_mesh_nodal_elmts_get(pmn,
+                                                                                      geom_kind);
+    int n_group = PDM_part_mesh_nodal_elmts_n_group_get(pmne);
+
+    if (n_group == 0) {
+      // Add volume groups
+      n_group = 3;
+
+      PDM_part_mesh_nodal_elmts_n_group_set(pmne, n_group);
+
+      int         **group_elt          = NULL;
+      PDM_g_num_t **group_elt_ln_to_gn = NULL;
+      PDM_malloc(group_elt,          n_group, int         *);
+      PDM_malloc(group_elt_ln_to_gn, n_group, PDM_g_num_t *);
+
+      for (int i_part = 0; i_part < n_part; i_part++) {
+        int n_elt = PDM_part_mesh_nodal_elmts_n_elmts_get(pmne, i_part);
+
+        PDM_g_num_t *g_num = PDM_part_mesh_nodal_elmts_g_num_get_from_part(pmne,
+                                                                           i_part,
+                                                                           PDM_OWNERSHIP_KEEP);
+        int *group_elt_n = PDM_array_zeros_int(n_group);
+        for (int i_elt = 0; i_elt < n_elt; i_elt++) {
+          int i_group = g_num[i_elt] % n_group;
+          group_elt_n[i_group]++;
+        }
+
+        for (int i_group = 0; i_group < n_group; i_group++) {
+          PDM_malloc(group_elt         [i_group], group_elt_n[i_group], int        );
+          PDM_malloc(group_elt_ln_to_gn[i_group], group_elt_n[i_group], PDM_g_num_t);
+          group_elt_n[i_group] = 0;
+        }
+
+        for (int i_elt = 0; i_elt < n_elt; i_elt++) {
+          int i_group = g_num[i_elt] % n_group;
+          group_elt         [i_group][group_elt_n[i_group]] = i_elt+1;
+          group_elt_ln_to_gn[i_group][group_elt_n[i_group]] = 1 + (g_num[i_elt] - 1) / n_group;
+          group_elt_n[i_group]++;
+        }
+
+        for (int i_group = 0; i_group < n_group; i_group++) {
+          PDM_part_mesh_nodal_elmts_group_set(pmne,
+                                              i_part,
+                                              i_group,
+                                              group_elt_n       [i_group],
+                                              group_elt         [i_group],
+                                              group_elt_ln_to_gn[i_group],
+                                              PDM_OWNERSHIP_KEEP);
+        }
+        PDM_free(group_elt_n);
+      }
+      PDM_free(group_elt         );
+      PDM_free(group_elt_ln_to_gn);
+    }
+  }
 
   return pmn;
 }
