9c5a84921e8637bbee6e22b2c592ca59f7226671
Clément Benazet <clement.benazet@onera.fr>
2025-10-30 18:06:25 +0100

    [part_geom] add unit tests for edge case

---
+++ src/ppart/test/pdm_part_geom.test.cpp
@@ -0,0 +1,263 @@
+#include <stddef.h>
+#include <vector>
+
+#include "doctest/doctest.h"
+#include "doctest/extensions/doctest_mpi.h"
+#include "pdm.h"
+#include "pdm_doctest.h"
+#include "pdm_logging.h"
+#include "pdm_mem_tool.h"
+#include "pdm_mpi.h"
+#include "pdm_vtk.h"
+#include "pdm_part_geom.h"
+
+
+MPI_TEST_CASE("[pdm_part_geom] vtx normals from edges", 2) {
+
+  /**
+   *                  2  (1)  4
+   *                  x-------x
+   *                  |
+   *                  | (3)
+   *                  |
+   *   rank0: x-------x 3
+   *          1  (2)
+   *
+   *                  x 1
+   *                  |
+   *                  | (2)
+   *                  |
+   *   rank1: x-------x
+   *          3  (1)  2
+   */
+
+  PDM_MPI_Comm comm = PDM_MPI_mpi_2_pdm_mpi_comm(&test_comm);
+
+  int i_rank;
+  PDM_MPI_Comm_rank(comm, &i_rank);
+
+
+  int n_part = 1;
+  int dimension = 1;
+
+  // > Mesh definition
+  int n_edge = (i_rank==0) ? 3 : 2;
+  int n_vtx  = (i_rank==0) ? 4 : 3;
+  std::vector<std::vector<int>> vedge_vtx_idx = {{0,2,4,6},
+                                                 {0,2,4  }};
+  std::vector<std::vector<int>> vedge_vtx = {{2,4, 1,3, 3,2},
+                                             {3,2, 2,1     }};
+  std::vector<std::vector<double>> vcoord = {{0.,0.,0.,
+                                              1.,1.,0.,
+                                              1.,0.,0.,
+                                              2.,1.,0.},
+                                             {1.,1.,0.,
+                                              1.,0.,0.,
+                                              0.,0.,0.}};
+  int    *edge_vtx_idx = vedge_vtx_idx[i_rank].data();
+  int    *edge_vtx     = vedge_vtx    [i_rank].data();
+  double *coord        = vcoord       [i_rank].data();
+
+  // > Part comm graph definition
+  int n_edge_graph = 2;
+  std::vector<std::vector<int>> vedge_graph = {{3,1,1,2, 2,1,1,1},
+                                               {1,0,1,2, 2,0,1,3}};
+  int *edge_graph = vedge_graph[i_rank].data();
+  PDM_part_comm_graph_t *pcg_edge = PDM_part_comm_graph_create(
+    n_part,
+   &n_edge_graph,
+   &edge_graph,
+    PDM_OWNERSHIP_USER,
+    comm
+  );
+
+  int n_vtx_graph = 3;
+  std::vector<std::vector<int>> vvtx_graph = {{1,1,1,3, 2,1,1,1, 3,1,1,2},
+                                              {1,0,1,2, 2,0,1,3, 3,0,1,1}};
+  int *vtx_graph = vvtx_graph[i_rank].data();
+  PDM_part_comm_graph_t *pcg_vtx = PDM_part_comm_graph_create(
+    n_part,
+   &n_vtx_graph,
+   &vtx_graph,
+    PDM_OWNERSHIP_USER,
+    comm
+  );
+
+
+  SUBCASE("full") {
+
+    int n_selected_edge = n_edge;
+    int n_selected_vtx  = n_vtx;
+    double **vtx_normal = NULL;
+    PDM_part_geom_vtx_normal_compute(
+      comm,
+      n_part,
+      dimension,
+     &n_selected_edge,
+      NULL,
+     &edge_vtx_idx,
+     &edge_vtx,
+      pcg_edge,
+     &n_selected_vtx,
+      NULL,
+     &coord,
+      pcg_vtx,
+     &vtx_normal
+    );
+
+    std::vector<std::vector<double>> expected_vtx_normal = {{0.      , -1.      , 0.,
+                                                            0.707107, -0.707107, 0.,
+                                                            0.707107, -0.707107, 0.,
+                                                            0.      , -1.      , 0.},
+                                                            {0.707107, -0.707107, 0.,
+                                                            0.707107, -0.707107, 0.,
+                                                            0.      , -1.      , 0.}};
+
+    for (int i_vtx=0; i_vtx<n_selected_vtx; ++i_vtx) {
+      CHECK(vtx_normal[0][3*i_vtx  ] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx  ]).epsilon(0.00001));
+      CHECK(vtx_normal[0][3*i_vtx+1] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx+1]).epsilon(0.00001));
+      CHECK(vtx_normal[0][3*i_vtx+2] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx+2]).epsilon(0.00001));
+    }
+
+    free(vtx_normal[0]);
+    free(vtx_normal);
+  }
+
+
+  SUBCASE("partial edge") {
+
+    int n_selected_edge = 1;
+    int n_selected_vtx  = n_vtx;
+    std::vector<std::vector<int>> vselected_edge = {{3},
+                                                    {2}}; // what if it is not same edge selected ?
+    int *selected_edge = vselected_edge[i_rank].data();
+
+    double **vtx_normal = NULL;
+    PDM_part_geom_vtx_normal_compute(
+      comm,
+      n_part,
+      dimension,
+     &n_selected_edge,
+     &selected_edge,
+     &edge_vtx_idx,
+     &edge_vtx,
+      pcg_edge,
+     &n_selected_vtx,
+      NULL,
+     &coord,
+      pcg_vtx,
+     &vtx_normal
+    );
+
+    std::vector<std::vector<double>> expected_vtx_normal = {{0., 0., 0.,
+                                                             1., 0., 0.,
+                                                             1., 0., 0.,
+                                                             0., 0., 0.},
+                                                            {1., 0., 0.,
+                                                             1., 0., 0.,
+                                                             0., 0., 0.}};
+
+    for (int i_vtx=0; i_vtx<n_selected_vtx; ++i_vtx) {
+      CHECK(vtx_normal[0][3*i_vtx  ] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx  ]).epsilon(0.00001));
+      CHECK(vtx_normal[0][3*i_vtx+1] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx+1]).epsilon(0.00001));
+      CHECK(vtx_normal[0][3*i_vtx+2] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx+2]).epsilon(0.00001));
+    }
+
+    free(vtx_normal[0]);
+    free(vtx_normal);
+  }
+
+
+  SUBCASE("partial vtx") {
+
+    int n_selected_edge = n_edge;
+    int n_selected_vtx  = 2;
+    std::vector<std::vector<int>> vselected_vtx = {{2,1},
+                                                   {1,3}}; // what if it is not same vtx selected ?
+    int *selected_vtx = vselected_vtx[i_rank].data();
+
+    double **vtx_normal = NULL;
+    PDM_part_geom_vtx_normal_compute(
+      comm,
+      n_part,
+      dimension,
+     &n_selected_edge,
+      NULL,
+     &edge_vtx_idx,
+     &edge_vtx,
+      pcg_edge,
+     &n_selected_vtx,
+     &selected_vtx,
+     &coord,
+      pcg_vtx,
+     &vtx_normal
+    );
+
+    std::vector<std::vector<double>> expected_vtx_normal = {{0.707107, -0.707107, 0.,
+                                                             0.      , -1.      , 0.},
+                                                            {0.707107, -0.707107, 0.,
+                                                             0.      , -1.      , 0.}};
+
+    for (int i_vtx=0; i_vtx<n_selected_vtx; ++i_vtx) {
+      CHECK(vtx_normal[0][3*i_vtx  ] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx  ]).epsilon(0.00001));
+      CHECK(vtx_normal[0][3*i_vtx+1] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx+1]).epsilon(0.00001));
+      CHECK(vtx_normal[0][3*i_vtx+2] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx+2]).epsilon(0.00001));
+    }
+
+    free(vtx_normal[0]);
+    free(vtx_normal);
+  }
+
+
+  SUBCASE("partial edge and vtx") {
+
+    int n_selected_edge = 1;
+    int n_selected_vtx  = 2;
+    std::vector<std::vector<int>> vselected_edge = {{3},
+                                                    {2}}; // what if it is not same edge selected ?
+    std::vector<std::vector<int>> vselected_vtx = {{2,1},
+                                                   {1,3}}; // what if it is not same vtx selected ?
+    int *selected_edge = vselected_edge[i_rank].data();
+    int *selected_vtx  = vselected_vtx[i_rank].data();
+
+    double **vtx_normal = NULL;
+    PDM_part_geom_vtx_normal_compute(
+      comm,
+      n_part,
+      dimension,
+     &n_selected_edge,
+     &selected_edge,
+     &edge_vtx_idx,
+     &edge_vtx,
+      pcg_edge,
+     &n_selected_vtx,
+     &selected_vtx,
+     &coord,
+      pcg_vtx,
+     &vtx_normal
+    );
+
+    /**
+     * Result can be strange because some referenced vertices won't have
+     * edge contribution.
+     * Is this ok, or should we check selected_edge/selected_vtx coherence ?
+     */
+    std::vector<std::vector<double>> expected_vtx_normal = {{1., 0., 0.,
+                                                             0., 0., 0.},
+                                                            {1., 0., 0.,
+                                                             0., 0., 0.}};
+
+    for (int i_vtx=0; i_vtx<n_selected_vtx; ++i_vtx) {
+      CHECK(vtx_normal[0][3*i_vtx  ] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx  ]).epsilon(0.00001));
+      CHECK(vtx_normal[0][3*i_vtx+1] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx+1]).epsilon(0.00001));
+      CHECK(vtx_normal[0][3*i_vtx+2] == doctest::Approx(expected_vtx_normal[i_rank][3*i_vtx+2]).epsilon(0.00001));
+    }
+
+    free(vtx_normal[0]);
+    free(vtx_normal);
+  }
+
+
+  PDM_part_comm_graph_free(pcg_edge);
+  PDM_part_comm_graph_free(pcg_vtx);
+}
