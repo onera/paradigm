4e50d5a095fe6fca4a58b110726b95996259ab0b
Bruno Maugars <bruno.maugars@onera.fr>
2025-12-07 16:49:22 +0100

    [part_mesh_nodal] Update with geom_kind rather than mesh_entities

--- src/ppart/pdm_multipart.c
+++ src/ppart/pdm_multipart.c
@@ -1074,7 +1074,7 @@ _compute_part_mesh_nodal_1d
   PDM_free(pn_vtx);
 
   /* Create part_comm_graph with gnum */
-  PDM_part_mesh_nodal_part_comm_graph_compute_from_gnum(pmn, PDM_MESH_ENTITY_VTX);
+  PDM_part_mesh_nodal_vtx_part_comm_graph_compute_from_gnum(pmn);
 
   return pmn;
 }
