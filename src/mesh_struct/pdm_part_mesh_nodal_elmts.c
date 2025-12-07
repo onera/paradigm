b607b124b87a62b298502c363a5e4e66c6298dbf
Bruno Maugars <bruno.maugars@onera.fr>
2025-12-07 07:41:07 +0100

    [part_mesh_nodal] Enhance method group_set, can be reallocated without memory leaks

--- src/mesh_struct/pdm_part_mesh_nodal_elmts.c
+++ src/mesh_struct/pdm_part_mesh_nodal_elmts.c
@@ -5041,10 +5041,17 @@ PDM_part_mesh_nodal_elmts_n_group_set
 
   pmne->n_group = n_group;
   for(int i_part = 0; i_part < pmne->n_part; ++i_part) {
-    PDM_malloc(pmne->n_group_elmt   [i_part], n_group, int              );
-    PDM_malloc(pmne->group_elmt     [i_part], n_group, int             *);
-    PDM_malloc(pmne->group_ln_to_gn [i_part], n_group, PDM_g_num_t     *);
-    PDM_malloc(pmne->ownership_group[i_part], n_group, PDM_ownership_t  );
+    if(pmne->n_group_elmt[i_part] == NULL) {
+      PDM_malloc(pmne->n_group_elmt   [i_part], n_group, int              );
+      PDM_malloc(pmne->group_elmt     [i_part], n_group, int             *);
+      PDM_malloc(pmne->group_ln_to_gn [i_part], n_group, PDM_g_num_t     *);
+      PDM_malloc(pmne->ownership_group[i_part], n_group, PDM_ownership_t  );
+    } else {
+      PDM_realloc(pmne->n_group_elmt   [i_part], pmne->n_group_elmt   [i_part], n_group, int              );
+      PDM_realloc(pmne->group_elmt     [i_part], pmne->group_elmt     [i_part], n_group, int             *);
+      PDM_realloc(pmne->group_ln_to_gn [i_part], pmne->group_ln_to_gn [i_part], n_group, PDM_g_num_t     *);
+      PDM_realloc(pmne->ownership_group[i_part], pmne->ownership_group[i_part], n_group, PDM_ownership_t  );
+    }
 
     for(int i_group = 0; i_group < pmne->n_group; ++i_group) {
       pmne->n_group_elmt   [i_part][i_group] = 0;
