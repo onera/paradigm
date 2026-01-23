8d5e076548f0159caef12e7fbba292be7cf7999e
Bruno Maugars <bruno.maugars@onera.fr>
2026-01-23 22:25:56 +0100

    [linear_algebra] rm useless const

--- src/util/pdm_linear_algebra.h
+++ src/util/pdm_linear_algebra.h
@@ -159,9 +159,9 @@ void PDM_linear_algebra_eig_sym2
  */
 void PDM_linear_algebra_eig_sym3
 (
-  const double a  [6],
-        double val[3],
-        double vec[9]
+  double a  [6],
+  double val[3],
+  double vec[9]
 );
 
 
