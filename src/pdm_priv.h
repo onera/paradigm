16206a76dbd5b573def60a3797637a9a2ba7c784
Bruno Maugars <bruno.maugars@onera.fr>
2026-04-21 06:21:31 +0200

    [pdm_priv] Add INTEL_LLVM warning management push/pop

--- src/pdm_priv.h
+++ src/pdm_priv.h
@@ -34,6 +34,20 @@ extern "C" {
 #define PDM_INTEL_SUPPRESS_WARNING_WITH_PUSH(w)
 #endif // PDM_INTEL
 
+#if defined(__INTEL_LLVM_COMPILER)
+#define PDM_PRAGMA_TO_STR(x) _Pragma(#x)
+#define PDM_INTEL_LLVM_SUPPRESS_WARNING_PUSH _Pragma("warning(push)")
+#define PDM_INTEL_LLVM_SUPPRESS_WARNING(w) PDM_PRAGMA_TO_STR(warning(disable:w))
+#define PDM_INTEL_LLVM_SUPPRESS_WARNING_POP _Pragma("warning(pop)")
+#define PDM_INTEL_LLVM_SUPPRESS_WARNING_WITH_PUSH(w)                                                \
+    PDM_INTEL_LLVM_SUPPRESS_WARNING_PUSH PDM_INTEL_LLVM_SUPPRESS_WARNING(w)
+#else // PDM_INTEL
+#define PDM_INTEL_LLVM_SUPPRESS_WARNING_PUSH
+#define PDM_INTEL_LLVM_SUPPRESS_WARNING(w)
+#define PDM_INTEL_LLVM_SUPPRESS_WARNING_POP
+#define PDM_INTEL_LLVM_SUPPRESS_WARNING_WITH_PUSH(w)
+#endif // PDM_INTEL_LLVM
+
 #if defined(__clang__)
 #define PDM_PRAGMA_TO_STR(x) _Pragma(#x)
 #define PDM_CLANG_SUPPRESS_WARNING_PUSH _Pragma("clang diagnostic push")
@@ -48,7 +62,7 @@ extern "C" {
 #define PDM_CLANG_SUPPRESS_WARNING_WITH_PUSH(w)
 #endif // PDM_CLANG
 
-#if defined(__GNUC__) && !defined(__INTEL_COMPILER)
+#if defined(__GNUC__) && (!defined(__INTEL_COMPILER) && !defined(__INTEL_LLVM_COMPILER))
 #define PDM_PRAGMA_TO_STR(x) _Pragma(#x)
 #define PDM_GCC_SUPPRESS_WARNING_PUSH _Pragma("GCC diagnostic push")
 #define PDM_GCC_SUPPRESS_WARNING(w) PDM_PRAGMA_TO_STR(GCC diagnostic ignored w)
