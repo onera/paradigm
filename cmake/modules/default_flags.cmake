b215acd8b00c45ac98b5536786da0eb66d9e5c5f
Bruno Maugars <bruno.maugars@onera.fr>
2026-05-14 09:58:24 +0200

    [compilation_flags] Update for intel-llvm

--- cmake/modules/default_flags.cmake
+++ cmake/modules/default_flags.cmake
@@ -403,7 +403,7 @@ elseif (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
   set (CXX_LIBRARIES_FLAG        )
 
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "IntelLLVM")
-  set (CMAKE_CXX_FLAGS "-fPIC -std=gnu++11 -Wall -pedantic -Wshadow -Wpointer-arith -Wuninitialized -Wunused -Wempty-translation-unit -Wno-unused-function -Wtautological-constant-compare")
+  set (CMAKE_CXX_FLAGS "-fPIC -std=gnu++11 -Wall -pedantic -Wshadow -Wpointer-arith -Wuninitialized -Wunused -Wempty-translation-unit -Wno-unused-function -Wtautological-constant-compare -Wno-zero-length-array")
   set (CMAKE_CXX_FLAGS_RELEASE "-O3")
   set (CMAKE_CXX_FLAGS_DEBUG "-g -O0")
   set (CMAKE_CXX_FLAGS_PROFILING       "${CMAKE_CXX_FLAGS_RELEASE} -p")
