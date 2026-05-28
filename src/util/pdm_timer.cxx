d49870d9ddf0f55efa923827fa749900259e292d
Bruno Maugars <bruno.maugars@onera.fr>
2026-05-28 14:13:29 +0200

    [misc] Fix pedantic ci

--- src/util/pdm_timer.cxx
+++ src/util/pdm_timer.cxx
@@ -43,13 +43,6 @@
 
 /*----------------------------------------------------------------------------*/
 
-#ifdef __cplusplus
-extern "C" {
-#if 0
-} /* Fake brace to force back Emacs auto-indentation back to column 0 */
-#endif
-#endif /* __cplusplus */
-
 /*============================================================================
  * Definition des types locaux
  *============================================================================*/
@@ -850,6 +843,13 @@ _dump_global_hierarchical_json
 /*============================================================================
  * Definition des fonctions publiques
  *============================================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#if 0
+} /* Fake brace to force back Emacs auto-indentation back to column 0 */
+#endif
+#endif /* __cplusplus */
 
 PDM_timer_t*
 PDM_timer_create
